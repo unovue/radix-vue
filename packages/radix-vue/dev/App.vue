56e87df6d79a5566c1c6eb432cfecc6803af71ba
Mujahid Anuar <mujahidfanuar@gmail.com>
2023-06-23 21:50:15 +0800

    Refactor monorepo

---
+++ packages/radix-vue/dev/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {
+  CoolCounter,
+  CoolButton,
+  type CoolButtonProps,
+  type CoolButtonColor,
+} from "../src";
+
+const bgColor: CoolButtonColor = "blue";
+</script>
+
+<template>
+  <CoolCounter :startingCount="10" />
+  <CoolButton :backgroundColor="bgColor">
+    Hello, I'm a cool button!
+  </CoolButton>
+</template>
--- packages/radix-vue/morph.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-	ui: {
-		accordion: {
-			trigger: "",
-		},
-	},
-};

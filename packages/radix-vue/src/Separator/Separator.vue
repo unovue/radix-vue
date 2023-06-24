5f0b354e750565ce63e6ab10cc51b879cd8bca96
Khairul Haaziq <khairulhaaziq@Khairuls-MacBook-Air.local>
2023-06-24 12:05:04 +0800

    init separator

---
+++ packages/radix-vue/src/Separator/Separator.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+type Orientation = "horizontal" | "vertical";
+
+defineProps<{
+  orientation: Orientation;
+}>();
+</script>
+
+<template>
+  <div :data-orientation="props.orientation" role="separator"></div>
+</template>

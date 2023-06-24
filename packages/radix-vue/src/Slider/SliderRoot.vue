1e792b2fa9bcdb7f89253530365f2065b225923a
Khairul Haaziq <101852870+khairulhaaziq@users.noreply.github.com>
2023-06-24 21:47:06 +0800

    Feat: Slider (1/3) (#41)
    * proved keyboard navigatable, moving the slider with keyboard, focus when click,
    * havent test all API / manipulating API

---
+++ packages/radix-vue/src/Slider/SliderRoot.vue
@@ -0,0 +1,165 @@
+<script lang="ts">
+import type { Ref, InjectionKey, ComputedRef } from "vue";
+import type { DataOrientation, Direction } from "../shared/types";
+
+export interface SliderRootProps {
+  // defaultValue?: string;
+  // value?: string;
+  name: string;
+  disabled: boolean;
+  orientation?: DataOrientation;
+  dir?: Direction;
+  modelValue?: number;
+  inverted: boolean;
+  min: number;
+  max: number;
+  step: number;
+  extraStep: number;
+  minStepsBetweenThumbs: number;
+}
+
+export const SLIDER_INJECTION_KEY =
+  Symbol() as InjectionKey<SliderProvideValue>;
+
+export interface SliderProvideValue {
+  modelValue?: Readonly<Ref<number | undefined>>;
+  changeModelValue: (value: any) => void;
+  rootSliderElement: Ref<HTMLElement | undefined>;
+  orientation: DataOrientation;
+  dir?: Direction;
+  thumbOffset: Readonly<ComputedRef<number | undefined>>;
+  min: number;
+  max: number;
+  step: number;
+  thumbElement?: Ref<HTMLElement | undefined>;
+}
+</script>
+
+<script setup lang="ts">
+import { ref, toRef, provide, computed } from "vue";
+
+const props = withDefaults(defineProps<SliderRootProps>(), {
+  disabled: false,
+  orientation: "horizontal",
+  activationMode: "automatic",
+  inverted: false,
+  min: 0,
+  max: 100,
+  step: 1,
+  minStepsBetweenThumbs: 0,
+});
+
+const emits = defineEmits(["update:modelValue"]);
+
+const rootSliderElement = ref<HTMLElement>();
+const thumbElement = ref<HTMLElement>();
+
+const thumbOffset = computed(() => {
+  if (!props.modelValue) {
+    return 0;
+  } else {
+    if (props.modelValue < 50) {
+      return ((props.modelValue - 50) / 50) * 10;
+    } else {
+      return ((props.modelValue - 50) / 50) * -10;
+    }
+  }
+});
+
+provide<SliderProvideValue>(SLIDER_INJECTION_KEY, {
+  modelValue: toRef(() => props.modelValue),
+  changeModelValue: (value: any) => {
+    emits("update:modelValue", value);
+  },
+  rootSliderElement: rootSliderElement,
+  orientation: props.orientation,
+  dir: props.dir,
+  thumbOffset: thumbOffset,
+  min: props.min,
+  max: props.max,
+  step: props.step,
+  thumbElement: thumbElement,
+});
+
+function updateModelValue(value: number) {
+  emits("update:modelValue", convertToClosestStep(value, props.step));
+}
+
+let rootSliderRect: DOMRect;
+
+function changeValue(e: MouseEvent) {
+  e.preventDefault();
+  if (rootSliderElement.value) {
+    rootSliderRect = rootSliderElement.value.getBoundingClientRect();
+    if (
+      e.clientX - 10 - thumbOffset.value > rootSliderRect.left &&
+      e.clientX - 10 - thumbOffset.value <
+      rootSliderRect.left + rootSliderRect.width
+    ) {
+      updateModelValue(
+        Math.round(
+          ((e.clientX - 10 - thumbOffset.value - rootSliderRect.left) /
+            rootSliderRect.width) *
+          100
+        )
+      );
+    }
+
+    document.addEventListener("pointermove", pointermove);
+    document.addEventListener("pointerup", pointerup);
+
+    if (thumbElement.value) {
+      thumbElement.value.focus();
+    }
+  }
+}
+
+const pointermove = (e: PointerEvent) => {
+  if (thumbElement.value) {
+    thumbElement.value.focus();
+  }
+  if (
+    e.clientX - 10 - thumbOffset.value > rootSliderRect!.left &&
+    e.clientX - 10 - thumbOffset.value <
+    rootSliderRect!.left + rootSliderRect!.width
+  ) {
+    updateModelValue(
+      ((e.clientX - 10 - thumbOffset.value - rootSliderRect!.left) /
+        rootSliderRect!.width) *
+      100
+    );
+  }
+  if (e.clientX - 10 - thumbOffset.value <= rootSliderRect!.left) {
+    updateModelValue(props.min);
+  }
+  if (
+    e.clientX - 10 - thumbOffset.value >=
+    rootSliderRect!.left + rootSliderRect!.width
+  ) {
+    updateModelValue(props.max);
+  }
+};
+
+const pointerup = (e: PointerEvent) => {
+  document.removeEventListener("pointermove", pointermove);
+  document.removeEventListener("pointerup", pointerup);
+};
+
+function convertToClosestStep(number: number, step: number) {
+  const quotient = Math.floor(number / step);
+  const remainder = number % step;
+
+  if (remainder <= step / 2) {
+    return quotient * step;
+  } else {
+    return (quotient + 1) * step;
+  }
+}
+</script>
+
+<template>
+  <span ref="rootSliderElement" @pointerdown="changeValue">
+    <slot />
+    <input style="display: none" :value="props.modelValue" :aria-valuenow="props.modelValue" :name="props.name" />
+  </span>
+</template>
---
+++ packages/radix-vue/src/Slider/index.ts
@@ -0,0 +1,4 @@
+export { default as SliderRoot } from "./SliderRoot.vue";
+export { default as SliderThumb } from "./SliderThumb.vue";
+export { default as SliderTrack } from "./SliderTrack.vue";
+export { default as SliderRange } from "./SliderRange.vue";

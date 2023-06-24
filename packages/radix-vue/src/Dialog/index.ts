57ed4179f5f330442fee6650276347be137d0a01
khairulhaaziq <rhycoz@yahoo.com>
2023-06-25 00:35:42 +0800

    add title description

--- packages/radix-vue/src/Dialog/index.ts
+++ packages/radix-vue/src/Dialog/index.ts
@@ -4,3 +4,5 @@ export { default as DialogPortal } from "./DialogPortal.vue";
 export { default as DialogContent } from "./DialogContent.vue";
 export { default as DialogOverlay } from "./DialogOverlay.vue";
 export { default as DialogClose } from "./DialogClose.vue";
+export { default as DialogTitle } from "./DialogTitle.vue";
+export { default as DialogDescription } from "./DialogDescription.vue";
--- packages/radix-vue/src/index.ts
+++ packages/radix-vue/src/index.ts
@@ -25,4 +25,6 @@ export {
   DialogClose,
   DialogPortal,
   DialogOverlay,
+  DialogTitle,
+  DialogDescription,
 } from "./Dialog";

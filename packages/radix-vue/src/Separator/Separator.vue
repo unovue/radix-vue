3d504fb8eb0d1379c9bdf2e4f5c592f0b4bb0d2d
Khairul Haaziq <khairulhaaziq@Khairuls-MacBook-Air.local>
2023-06-24 12:16:00 +0800

    separator 1/3

--- packages/radix-vue/src/Separator/Separator.vue
+++ packages/radix-vue/src/Separator/Separator.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 type Orientation = "horizontal" | "vertical";
 
-defineProps<{
-  orientation: Orientation;
-}>();
+const props = defineProps({
+  orientation: {
+    type: String as Orientation,
+    required: false,
+    default: "horizontal",
+  },
+  decorative: {
+    type: Boolean,
+    required: false,
+  },
+});
 </script>
 
 <template>
-  <div :data-orientation="props.orientation" role="separator"></div>
+  <div
+    :data-orientation="props.orientation"
+    :role="`${decorative ? 'none' : 'separator'}`"
+  ></div>
 </template>
--- packages/radix-vue/src/index.ts
+++ packages/radix-vue/src/index.ts
@@ -2,3 +2,4 @@
 export { default as HelloButton } from "./HelloButton.vue";
 export { CheckboxIndicator, CheckboxRoot } from "./Checkbox";
 export { SwitchRoot, SwitchThumb } from "./Switch";
+export { Separator } from "./Separator";

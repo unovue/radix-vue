dc5525bfaa23c233366c3b3f4ac537dc3c68b4e4
Khairul Haaziq <khairulhaaziq@Khairuls-MacBook-Air.local>
2023-06-24 12:29:08 +0800

    label 3/3

--- packages/radix-vue/src/index.ts
+++ packages/radix-vue/src/index.ts
@@ -3,3 +3,4 @@ export { default as HelloButton } from "./HelloButton.vue";
 export { CheckboxIndicator, CheckboxRoot } from "./Checkbox";
 export { SwitchRoot, SwitchThumb } from "./Switch";
 export { Separator } from "./Separator";
+export { Label } from "./Label";

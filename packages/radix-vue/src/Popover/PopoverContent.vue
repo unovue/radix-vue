439ffe379f4318f616f5337bc0686153289c1e8f
khairulhaaziq <rhycoz@yahoo.com>
2023-06-25 19:11:43 +0800

    disable button focus click outside

--- packages/radix-vue/src/Popover/PopoverContent.vue
+++ packages/radix-vue/src/Popover/PopoverContent.vue
@@ -48,8 +48,6 @@ watchEffect(() => {
 function closeDialogWhenClickOutside(e: MouseEvent) {
   const clickOutside = useClickOutside(e, tooltipContentElement.value);
   if (clickOutside) {
-    e.preventDefault();
-    e.stopPropagation();
     injectedValue.hideTooltip();
   }
 }

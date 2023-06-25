5f9aeba9f5c33ddf553f015755bc5c4085d28439
khairulhaaziq <rhycoz@yahoo.com>
2023-06-25 19:24:53 +0800

    init branch dropdownmenu

---
+++ packages/radix-vue/src/DropdownMenu/PopoverArrow.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { inject, ref, onMounted } from "vue";
+import {
+  POPOVER_INJECTION_KEY,
+  type PopoverProvideValue,
+} from "./PopoverRoot.vue";
+
+const injectedValue = inject<PopoverProvideValue>(POPOVER_INJECTION_KEY);
+
+const props = defineProps({
+  class: String,
+});
+
+const arrowElement = ref<HTMLElement>();
+onMounted(() => {
+  injectedValue!.arrowElement.value = arrowElement.value;
+});
+</script>
+
+<template>
+  <span ref="arrowElement" style="position: absolute">
+    <svg
+      :class="props.class"
+      width="10"
+      height="5"
+      viewBox="0 0 30 10"
+      preserveAspectRatio="none"
+      style="display: block"
+    >
+      <polygon points="0,0 30,0 15,10"></polygon>
+    </svg>
+  </span>
+</template>

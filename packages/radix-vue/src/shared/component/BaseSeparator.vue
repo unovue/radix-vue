8e7201aadb9a9f28aa1f2416d5d9724b55d6e9ce
khairulhaaziq <rhycoz@yahoo.com>
2023-06-26 16:35:13 +0800

    init

---
+++ packages/radix-vue/src/shared/component/BaseSeparator.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import type { DataOrientation } from "../types";
+
+export interface BaseSeparatorProps {
+  orientation?: DataOrientation;
+  decorative?: boolean;
+}
+</script>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<BaseSeparatorProps>(), {
+  orientation: "horizontal"
+})''
+</script>
+
+<template>
+  <div
+    :data-orientation="props.orientation"
+    :role="`${decorative ? 'none' : 'separator'}`"
+  ></div>
+</template>

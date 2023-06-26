fa949eb77101178e351308edb586c9cd47c22e37
khairulhaaziq <rhycoz@yahoo.com>
2023-06-26 15:48:43 +0800

    add tooltip

--- packages/radix-vue/src/Popover/PopoverArrow.vue
+++ packages/radix-vue/src/Popover/PopoverArrow.vue
@@ -1,33 +1,73 @@
+<script lang="ts">
+interface PopoverArrowProps {
+  size?: number;
+}
+</script>
+
 <script setup lang="ts">
-import { inject, ref, onMounted } from "vue";
+import type { Side } from "@floating-ui/dom";
+import { inject, ref, onMounted, computed } from "vue";
 import {
   POPOVER_INJECTION_KEY,
   type PopoverProvideValue,
 } from "./PopoverRoot.vue";
+import {
+  POPOVER_CONTENT_INJECTION_KEY,
+  type PopoverContentProvideValue,
+} from "./PopoverContent.vue";
+
+const props = withDefaults(defineProps<PopoverArrowProps>(), {
+  size: 10,
+});
 
 const injectedValue = inject<PopoverProvideValue>(POPOVER_INJECTION_KEY);
 
-const props = defineProps({
-  class: String,
-});
+const injectedContentValue = inject<PopoverContentProvideValue>(
+  POPOVER_CONTENT_INJECTION_KEY
+);
 
 const arrowElement = ref<HTMLElement>();
 onMounted(() => {
   injectedValue!.arrowElement.value = arrowElement.value;
+  console.log(injectedValue?.middlewareData);
+});
+
+const arrowX = computed<number>(() => {
+  return injectedContentValue?.middlewareData.value?.arrow?.x ?? 0;
+});
+const arrowY = computed<number>(() => {
+  return injectedContentValue?.middlewareData.value?.arrow?.y ?? 0;
+});
+const offset = computed<number>(() => {
+  return -props.size / 2;
+});
+
+const position = computed<Side>(() => {
+  const opposedSide: Record<Side, Side> = {
+    left: "right",
+    right: "left",
+    bottom: "top",
+    top: "bottom",
+  };
+  return opposedSide[injectedContentValue?.floatPosition.value as Side];
 });
 </script>
 
 <template>
-  <span ref="arrowElement" style="position: absolute">
-    <svg
-      :class="props.class"
-      width="10"
-      height="5"
-      viewBox="0 0 30 10"
-      preserveAspectRatio="none"
-      style="display: block"
-    >
-      <polygon points="0,0 30,0 15,10"></polygon>
-    </svg>
-  </span>
+  <div
+    ref="arrowElement"
+    :style="{
+      left: arrowX ? `${arrowX}px` : '',
+      top: arrowY ? `${arrowY}px` : '',
+      [position]: `${offset}px`,
+      width: `${props.size}px`,
+    }"
+    style="
+      position: absolute;
+      aspect-ratio: 1;
+      transform: rotate(45deg);
+      z-index: -1;
+      pointer-events: none;
+    "
+  ></div>
 </template>

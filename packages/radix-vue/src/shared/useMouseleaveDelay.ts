657dfde7603fe60ed0cc3ca92b8d0391639d9254
khairulhaaziq <rhycoz@yahoo.com>
2023-06-28 06:58:01 +0800

    enable leavedelay, allow portal to open when hovered

---
+++ packages/radix-vue/src/shared/useMouseleaveDelay.ts
@@ -0,0 +1,34 @@
+export const useMouseleaveDelay = (
+  mouseleaveEvent: MouseEvent,
+  duration = 300
+) => {
+  let isHovered = true;
+  let timeoutId: number | undefined;
+  let resolvePromise: (value: boolean) => void;
+
+  const element = mouseleaveEvent.target as HTMLElement;
+
+  const timeoutPromise = new Promise<boolean>((resolve) => {
+    resolvePromise = resolve;
+    timeoutId = setTimeout(() => {
+      cleanupEvents();
+      resolve(isHovered);
+    }, duration);
+  });
+
+  function handleMouseenter() {
+    isHovered = false;
+    cleanupEvents();
+    clearTimeout(timeoutId);
+    timeoutId = undefined;
+    resolvePromise(false);
+  }
+
+  function cleanupEvents() {
+    element.removeEventListener("mouseenter", handleMouseenter);
+  }
+
+  element.addEventListener("mouseenter", handleMouseenter);
+
+  return timeoutPromise;
+};

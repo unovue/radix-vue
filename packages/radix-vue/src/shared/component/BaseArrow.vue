88955a162c57479796b5c42bd2442de876e3c89b
khairulhaaziq <rhycoz@yahoo.com>
2023-06-28 01:50:19 +0800

    setel

--- packages/radix-vue/src/shared/component/BaseArrow.vue
+++ packages/radix-vue/src/shared/component/BaseArrow.vue
@@ -1,36 +1,32 @@
 <script lang="ts">
-import type { TooltipProvideValue } from "./TooltipRoot.vue";
-import type { TooltipContentProvideValue } from "./TooltipContent.vue";
+import type { TooltipProvideValue } from "../../Tooltip/TooltipRoot.vue";
+import type { TooltipContentProvideValue } from "../../Tooltip/TooltipContent.vue";
 
 interface TooltipArrowProps {
   size?: number;
-  injectedValue?: TooltipProvideValue;
-  injectedContentValue?: TooltipContentProvideValue
+  injectedValue?: TooltipProvideValue | undefined;
+  injectedContentValue?: TooltipContentProvideValue | undefined;
 }
 </script>
 
 <script setup lang="ts">
 import type { Side } from "@floating-ui/dom";
-import { inject, ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed } from "vue";
 
 const props = withDefaults(defineProps<TooltipArrowProps>(), {
   size: 10,
 });
 
-const injectedValue = props.injectedValue;
-
-const injectedContentValue = props.injectedContentValue;
-
 const arrowElement = ref<HTMLElement>();
 onMounted(() => {
-  injectedValue!.arrowElement.value = arrowElement.value;
+  props.injectedValue!.arrowElement.value = arrowElement.value;
 });
 
 const arrowX = computed<number>(() => {
-  return injectedContentValue?.middlewareData.value?.arrow?.x ?? 0;
+  return props.injectedContentValue?.middlewareData.value?.arrow?.x ?? 0;
 });
 const arrowY = computed<number>(() => {
-  return injectedContentValue?.middlewareData.value?.arrow?.y ?? 0;
+  return props.injectedContentValue?.middlewareData.value?.arrow?.y ?? 0;
 });
 const offset = computed<number>(() => {
   return -props.size / 2;
@@ -43,7 +39,7 @@ const position = computed<Side>(() => {
     bottom: "top",
     top: "bottom",
   };
-  return opposedSide[injectedContentValue?.floatPosition.value as Side];
+  return opposedSide[props.injectedContentValue?.floatPosition.value as Side];
 });
 </script>
 

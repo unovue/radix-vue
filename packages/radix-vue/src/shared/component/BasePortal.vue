ef47259f98a56e4567d60e229c4796f5c9b6079f
Khairul Haaziq <101852870+khairulhaaziq@users.noreply.github.com>
2023-06-27 21:47:35 +0800

    enable hovercard without v-model (#68)

---
+++ packages/radix-vue/src/shared/component/BasePortal.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export interface BasePortalProps {
+  to?: string | HTMLElement;
+}
+</script>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<BasePortalProps>(), {
+  to: "body",
+});
+</script>
+
+<template>
+  <Teleport :to="props.to">
+    <slot />
+  </Teleport>
+</template>

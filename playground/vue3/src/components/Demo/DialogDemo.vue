401cedf4dbcf240a8e8793ddc9bb217908b468c1
zernonia <zernonia@gmail.com>
2023-06-28 19:49:49 +0800

    chore: update demo

--- playground/vue3/src/components/Demo/DialogDemo.vue
+++ playground/vue3/src/components/Demo/DialogDemo.vue
@@ -8,7 +8,7 @@ import {
   DialogClose,
   DialogPortal,
   DialogTitle,
-  DialogDescription
+  DialogDescription,
 } from "radix-vue";
 import { Icon } from "@iconify/vue";
 
@@ -16,29 +16,23 @@ const dialogOpen = ref(false);
 </script>
 
 <template>
-  <DialogRoot v-model="dialogOpen">
+  <DialogRoot>
     <DialogTrigger
       class="text-violet11 shadow-blackA7 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
     >
       Edit profile
     </DialogTrigger>
     <DialogPortal>
-      <DialogOverlay
-        class="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0"
-      />
+      <DialogOverlay class="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
       <DialogContent
         class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
       >
-        <DialogTitle class="text-mauve12 m-0 text-[17px] font-medium">
-          Edit profile
-        </DialogTitle>
+        <DialogTitle class="text-mauve12 m-0 text-[17px] font-medium"> Edit profile </DialogTitle>
         <DialogDescription class="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
           Make changes to your profile here. Click save when you're done.
         </DialogDescription>
         <fieldset class="mb-[15px] flex items-center gap-5">
-          <label class="text-violet11 w-[90px] text-right text-[15px]" for="name">
-            Name
-          </label>
+          <label class="text-violet11 w-[90px] text-right text-[15px]" for="name"> Name </label>
           <input
             class="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
             id="name"
@@ -46,9 +40,7 @@ const dialogOpen = ref(false);
           />
         </fieldset>
         <fieldset class="mb-[15px] flex items-center gap-5">
-          <label class="text-violet11 w-[90px] text-right text-[15px]" for="username">
-            Username
-          </label>
+          <label class="text-violet11 w-[90px] text-right text-[15px]" for="username"> Username </label>
           <input
             class="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
             id="username"

51b0ffbda24d7223c63ece4413e99c0414036b7a
zernonia <zernonia@gmail.com>
2023-06-28 17:18:54 +0800

    chore: remove own useVmodel

--- packages/radix-vue/src/shared/index.ts
+++ packages/radix-vue/src/shared/index.ts
@@ -2,4 +2,3 @@ export { useArrowNavigation } from "./useArrowNavigation";
 export { useMouseInElement } from "./useMouseInElement";
 export { useHoverDelay } from "./useHoverDelay";
 export { useMouseleaveDelay } from "./useMouseleaveDelay";
-export { type UseVModel, useVModel } from "./useVModel";

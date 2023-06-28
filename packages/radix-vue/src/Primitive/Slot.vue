65ab9fc07090c0f5da136165936054323693f3ba
zernonia <zernonia@gmail.com>
2023-06-28 14:48:59 +0800

    fix: typo

--- packages/radix-vue/src/Primitive/Slot.vue
+++ packages/radix-vue/src/Primitive/Slot.vue
@@ -1,7 +1,7 @@
 <template>
   <component
     :is="
-      // @ts-ignore we cant ignore this as we have validated the existence of 1 child in Primitive.vue
+      // @ts-ignore we can ignore this as we have validated the existence of 1 child in Primitive.vue
       $slots.default?.()[0].children?.[0]
     "
     v-bind="$attrs"

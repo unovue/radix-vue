0a88902d6f7f084c24e63f83ab7ba2c1ebc6e48b
zernonia <zernonia@gmail.com>
2023-06-28 11:35:53 +0800

    feat: completed asChild prop via Primitive component

---
+++ packages/radix-vue/src/Primitive/Slot.vue
@@ -0,0 +1,9 @@
+<template>
+  <component
+    :is="
+      // @ts-ignore we cant ignore this as we have validated the existence of 1 child in Primitive.vue
+      $slots.default?.()[0].children?.[0]
+    "
+    v-bind="$attrs"
+  ></component>
+</template>

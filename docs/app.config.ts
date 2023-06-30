999e51f1d08bee38d1b93e135c259bc6bb322b7c
khairulhaaziq <rhycoz@yahoo.com>
2023-06-30 17:04:19 +0800

    new setup

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -4,8 +4,13 @@ export default defineAppConfig({
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      twitter: 'nuxt_js',
+      github: 'nuxt-themes/docus',
+      nuxt: {
+        label: 'Nuxt',
+        icon: 'simple-icons:nuxtdotjs',
+        href: 'https://nuxt.com'
+      }
     },
     github: {
       dir: '.starters/default/content',
@@ -28,14 +33,6 @@ export default defineAppConfig({
       showLinkIcon: true,
       exclude: [],
       fluid: true
-    },
-    footer: {
-      iconLinks: [
-        {
-          href: 'https://nuxt.com',
-          icon: 'simple-icons:nuxtdotjs'
-        }
-      ]
     }
   }
 })

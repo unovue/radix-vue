d2a7c23428ef5ed8d6d38725b2968689194953ac
khairulhaaziq <rhycoz@yahoo.com>
2023-07-04 21:28:55 +0800

    fix hover hashtag on headers

--- docs/components/content/prose/ProseH1.vue
+++ docs/components/content/prose/ProseH1.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import { Icon } from "@iconify/vue"
+
 defineProps<{ id: string }>();
 </script>
 
 <template>
   <h1 :id="id" class="scroll-mt-[205px] lg:scroll-mt-[156px] font-bold mb-2 !text-4xl mt-12">
     <NuxtLink :href="`#${id}`" class="group">
-      <div class="-ml-6 pr-2 py-2 inline-flex opacity-0 group-hover:opacity-100 transition-opacity absolute">
-        <Icon icon="heroicons:hashtag-20-solid" class="w-4 h-4 text-primary-500 dark:text-primary-400" />
+      <div class="-ml-6 pr-2 py-2 inline-flex opacity-0 group-hover:opacity-100 transition-opacity absolute h-10">
+        <Icon icon="heroicons:hashtag-20-solid" class="w-5 h-5 text-green-400 my-auto" />
       </div>
 
       <slot />

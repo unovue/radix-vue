6759e4f435f7634d5c597f487431781ffdabacbc
khairulhaaziq <rhycoz@yahoo.com>
2023-07-06 14:27:34 +0800

    fix overflow x scroll and background black

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -7,8 +7,8 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="flex flex-col items-center px-5 min-h-screen -mt-20">
-    <div class="z-[-1] bg-black fixed h-screen w-screen" />
+  <div class="flex flex-col items-center px-5 min-h-screen w-full max-w-screen overflow-x-clip -mt-20">
+    <div class="z-[-1] bg-black fixed h-[200vh] w-screen" />
     <div
       class="max-w-7xl relative w-full flex flex-col items-center overflow-y-clip h-full py-20"
     >

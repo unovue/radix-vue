e29a10757b780593022caa8dbf7b2ef7726c41a8
khairulhaaziq <rhycoz@yahoo.com>
2023-07-11 05:46:59 +0800

    fix demo styling

--- playground/vue3/src/components/Demo/SelectDemo.vue
+++ playground/vue3/src/components/Demo/SelectDemo.vue
@@ -22,6 +22,7 @@ function handleClick() {
 }
 
 const options = ["Apple", "Banana", "Blueberry", "Grapes", "Pineapple"]
+const vegetables = ["Aubergine", "Broccoli", "Carrot", "Courgette", "Leek"]
 </script>
 
 <template>
@@ -30,7 +31,7 @@ const options = ["Apple", "Banana", "Blueberry", "Grapes", "Pineapple"]
   </div>
   <SelectRoot v-model="fruit">
     <SelectTrigger
-      class="text-sm w-[180px] rounded-md px-3 h-10 flex items-center justify-between text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black"
+      class="w-[133px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
       aria-label="Customise options"
     >
       {{ fruit }}
@@ -39,16 +40,31 @@ const options = ["Apple", "Banana", "Blueberry", "Grapes", "Pineapple"]
 
     <SelectPortal>
       <SelectContent
-        class="w-[180px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+        class="w-[133px] bg-white rounded p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
         :sideOffset="5"
       >
 
-        <SelectLabel class="pl-[25px] text-sm leading-[25px] text-mauve11"> Fruits </SelectLabel>
+        <SelectLabel class="px-[25px] text-xs leading-[25px] text-mauve11"> Fruits </SelectLabel>
         <SelectRadioGroup>
           <SelectRadioItem
             v-for="option in options"
-            class="text-sm leading-none text-violet11 rounded-[3px] flex items-center h-8 px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+            class="text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
             :value="option"
+          >
+            <SelectItemIndicator class="absolute left-0 w-[25px] inline-flex items-center justify-center">
+              <Icon icon="radix-icons:check" />
+            </SelectItemIndicator>
+            {{option}}
+          </SelectRadioItem>
+        </SelectRadioGroup>
+        <SelectSeparator class="h-[1px] bg-violet6 m-[5px]"/>
+        <SelectLabel class="px-[25px] text-xs leading-[25px] text-mauve11"> Vegetables </SelectLabel>
+        <SelectRadioGroup>
+          <SelectRadioItem
+            v-for="option in vegetables"
+            class="text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+            :value="option"
+            :disabled="option === 'Courgette'"
           >
             <SelectItemIndicator class="absolute left-0 w-[25px] inline-flex items-center justify-center">
               <Icon icon="radix-icons:check" />

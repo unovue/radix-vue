1bb540d2210306cf3604fdf4420ae8a033dc58ec
npldevfr <asnpldev@gmail.com>
2023-07-10 15:35:54 +0200

    test(label): setup label tests

---
+++ packages/radix-vue/src/Label/label.test.ts
@@ -0,0 +1,10 @@
+import { render, fireEvent, screen } from "@testing-library/vue";
+import { test, it } from "vitest";
+import Label from "./Label.vue";
+
+test("test label", async () => {
+  // it("should render label", async () => {
+  //   render(Label, { props: { label: "test" } });
+  //   expect(screen.getByText("test")).toBeInTheDocument();
+  // });
+});
--- packages/radix-vue/vitest.config.ts
+++ packages/radix-vue/vitest.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vitest/config";
 import Vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+const r = (p: string) => resolve(__dirname, p);
 
 export default defineConfig({
   plugins: [Vue()],
+  resolve: {
+    alias: {
+      "@": r("./src"),
+    },
+  },
   test: {
     environment: "jsdom",
     exclude: ["**/node_modules/**"],

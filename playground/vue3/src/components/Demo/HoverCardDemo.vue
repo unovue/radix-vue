d9504af50c751b0381ca381ddb79064aaa84009e
khairulhaaziq <rhycoz@yahoo.com>
2023-07-10 18:10:52 +0800

    fix deps to enable dev server

--- playground/vue3/src/components/Demo/HoverCardDemo.vue
+++ playground/vue3/src/components/Demo/HoverCardDemo.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { HoverCardRoot, HoverCardTrigger, HoverCardPortal, HoverCardContent, HoverCardArrow } from "@/HoverCard";
+import { HoverCardRoot, HoverCardTrigger, HoverCardPortal, HoverCardContent, HoverCardArrow } from "radix-vue";
 
 const hoverState = ref(false);
 </script>

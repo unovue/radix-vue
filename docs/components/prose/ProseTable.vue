2bac85639629999c5f2717c9b584fffc63862b1e
khairulhaaziq <rhycoz@yahoo.com>
2023-07-14 09:06:25 +0800

    table margin

--- docs/components/prose/ProseTable.vue
+++ docs/components/prose/ProseTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="w-full !table border-b border-[var(--vp-c-divider)] border-x">
+  <table class="w-full !table border-b border-[var(--vp-c-divider)] border-x mb-12">
     <slot />
   </table>
 </template>

75addac8902687a2cf0b292a4f04fc32dad7abf1
zernonia <zernonia@gmail.com>
2023-07-15 23:21:23 +0800

    add focus outside composables

--- packages/radix-vue/src/shared/index.ts
+++ packages/radix-vue/src/shared/index.ts
@@ -1,3 +1,4 @@
+export { onFocusOutside } from "./onFocusOutside";
 export { useArrowNavigation } from "./useArrowNavigation";
 export { useMouseInElement } from "./useMouseInElement";
 export { useHoverDelay } from "./useHoverDelay";

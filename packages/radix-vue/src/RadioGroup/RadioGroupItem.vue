6dce4227f04b997597b2c7b1a26029a1e91e7198
maximo <maximo@nimiq.com>
2023-07-15 14:32:39 +0700

    RadioGroup: Updated API

--- packages/radix-vue/src/RadioGroup/RadioGroupItem.vue
+++ packages/radix-vue/src/RadioGroup/RadioGroupItem.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { inject, computed, provide } from "vue";
 import { PrimitiveButton, usePrimitiveElement } from "@/Primitive";
+import { computed, inject, provide, readonly } from "vue";
+import { useArrowNavigation } from "../shared";
 import {
   RADIO_GROUP_INJECTION_KEY,
+  RADIO_ITEM_INJECTION_KEY,
   type RadioGroupProvideValue,
 } from "./RadioGroupRoot.vue";
-import { useArrowNavigation } from "../shared";
 
 interface RadioGroupItemProps {
   asChild?: boolean;
@@ -18,15 +19,25 @@ const injectedValue = inject<RadioGroupProvideValue>(RADIO_GROUP_INJECTION_KEY);
 
 const props = withDefaults(defineProps<RadioGroupItemProps>(), {
   asChild: false,
+  disabled: false,
 });
 
-provide("radioItemValue", props.value);
-
-const state = computed(() => {
-  return injectedValue?.modelValue?.value === props.value ? "on" : "off";
+const disabled = computed(() => {
+  return injectedValue?.disabled.value || props.disabled;
 });
 
-function changeTab(value: string) {
+const required = computed(() => {
+  return injectedValue?.required.value || props.required;
+});
+
+const checked = computed(() => {
+  return injectedValue?.modelValue?.value === props.value;
+});
+
+provide(RADIO_ITEM_INJECTION_KEY, readonly({ disabled, checked }));
+
+function changeOption(value: string) {
+  if (disabled.value) return;
   injectedValue?.changeModelValue(value);
 }
 
@@ -34,32 +45,53 @@ const { primitiveElement, currentElement: currentRadioElement } =
   usePrimitiveElement();
 
 function handleKeydown(e: KeyboardEvent) {
+  if (disabled.value) return;
+
   const newSelectedElement = useArrowNavigation(
     e,
     currentRadioElement.value!,
-    injectedValue?.parentElement.value!
+    injectedValue?.parentElement.value!,
+    {
+      arrowKeyOptions: injectedValue?.orientation.value,
+      loop: injectedValue?.loop.value,
+    }
   );
 
   if (newSelectedElement) {
     newSelectedElement.focus();
-    changeTab(newSelectedElement?.getAttribute("data-radix-vue-radio-value")!);
+    changeOption(
+      newSelectedElement?.getAttribute("data-radix-vue-radio-value")!
+    );
+    injectedValue!.currentFocusedElement!.value = newSelectedElement;
   }
 }
+
+const getTabIndex = computed(() => {
+  if (!injectedValue?.currentFocusedElement?.value) {
+    return checked.value ? "0" : "-1";
+  } else
+    return injectedValue?.currentFocusedElement?.value ===
+      currentRadioElement.value
+      ? "0"
+      : "-1";
+});
 </script>
 
 <template>
   <PrimitiveButton
     type="button"
-    :data-state="state"
-    :data-disabled="props.disabled"
-    @click="injectedValue?.changeModelValue(props.value)"
-    :tabindex="`${
-      injectedValue?.modelValue?.value === props.value ? '0' : '-1'
-    }`"
     ref="primitiveElement"
-    @keydown="handleKeydown"
     data-radix-vue-collection-item
+    :asChild="props.asChild"
+    :data-state="checked ? 'checked' : 'unchecked'"
+    :required="required"
+    :disabled="disabled"
+    :data-disabled="disabled ? '' : undefined"
+    :tabindex="getTabIndex"
     :data-radix-vue-radio-value="props.value"
+    :name="injectedValue?.name"
+    @click="changeOption(props.value!)"
+    @keydown="handleKeydown"
   >
     <slot />
   </PrimitiveButton>

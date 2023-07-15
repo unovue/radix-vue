ee04ee5a7219bcf7b01411532d368954009cf7f3
khairulhaaziq <rhycoz@yahoo.com>
2023-07-15 19:39:04 +0800

    init

---
+++ playground/vue3/src/components/stories/Tabs.story.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TabsDemo from "../Demo/TabsDemo.vue"
+</script>
+
+<template>
+  <Story title="Cars" :layout="{ type: 'single', iframe: true }">
+    <Variant title="default">
+      <TabsDemo />
+    </Variant>
+  </Story>
+</template>

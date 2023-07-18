b920ce1540e20ba17fbfb712e089f24f28687a7b
zernonia <zernonia@gmail.com>
2023-07-18 10:17:53 +0800

    fix build issue

--- packages/radix-vue/vite.config.ts
+++ packages/radix-vue/vite.config.ts
@@ -31,8 +31,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library (Vue)
-      // we also exclude histoire files
-      external: ["vue", new RegExp(/\.story\.vue$/)],
+      external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps

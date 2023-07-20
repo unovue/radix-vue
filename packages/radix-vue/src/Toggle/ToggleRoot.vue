0713e988c73128aacfa192f08df14ac4adae4f60
Dennis Smuda <smuda.dennis@gmail.com>
2023-07-20 21:28:49 +0200

    feat: ToggleRoot setup

--- packages/radix-vue/src/Toggle/ToggleRoot.vue
+++ packages/radix-vue/src/Toggle/ToggleRoot.vue
@@ -1,25 +1,56 @@
 <script lang="ts">
-import PrimitiveProps from "@/Primitive";
-
-// export interface ToggleGroupRootProps extends PrimitiveProps {
-//   value?: string;
-//   defaultValue?: string;
-//   //onValueChange?: void;
-//   disabled?: boolean;
-//   rovingFocus?: boolean;
-//   loop: boolean;
-//   modelValue?: string | string[];
-// }
+export interface ToggleGroupRootProps extends PrimitiveProps {
+  value?: string;
+  defaultValue?: string;
+  //onValueChange?: void;
+  disabled?: boolean;
+  rovingFocus?: boolean;
+  loop: boolean;
+  modelValue?: string | string[];
+}
 </script>
 
-<script setup lang="st">
-import { computed } from "vue";
+<script setup lang="ts">
 import { useVModel } from "@vueuse/core";
+import { PrimitiveDiv, type PrimitiveProps } from "@/Primitive";
 
 const emits = defineEmits(["update:modelValue"]);
 
-const modelValue = useVModel(props, "modelValue", emits, {
+const props = withDefaults(defineProps<ToggleGroupRootProps>(), {
+  type: "single",
+  disabled: false,
+});
+
+const pressed = useVModel(props, "modelValue", emits, {
   defaultValue: props.defaultValue,
   passive: true,
 });
 </script>
+
+<template>
+  <PrimitiveDiv
+    :value="pressed"
+    role="checkbox"
+    :aria-checked="pressed"
+    :data-state="dataState"
+    :data-disabled="props.disabled"
+    :as-child="props.asChild"
+    style="position: relative"
+  >
+    <input
+      type="checkbox"
+      :id="props.id"
+      @keydown="handleKeydown"
+      v-bind="pressed"
+      @change="togglePressed"
+      :checked="pressed"
+      :name="props.name"
+      aria-hidden="true"
+      :disabled="props.disabled"
+      :data-state="dataState"
+      :data-disabled="props.disabled"
+      style="opacity: 0; position: absolute; inset: 0"
+    />
+    <slot />
+  </PrimitiveDiv>
+</template>

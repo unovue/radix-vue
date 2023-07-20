9e017835280f5a5aea4a886cb80d0c63109b2673
Dennis Smuda <smuda.dennis@gmail.com>
2023-07-20 21:17:38 +0200

    feat: init ToggleRoot component

---
+++ packages/radix-vue/src/Toggle/ToggleRoot.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import PrimitiveProps from "@/Primitive";
+
+// export interface ToggleGroupRootProps extends PrimitiveProps {
+//   value?: string;
+//   defaultValue?: string;
+//   //onValueChange?: void;
+//   disabled?: boolean;
+//   rovingFocus?: boolean;
+//   loop: boolean;
+//   modelValue?: string | string[];
+// }
+</script>
+
+<script setup lang="st">
+import { computed } from "vue";
+import { useVModel } from "@vueuse/core";
+
+const emits = defineEmits(["update:modelValue"]);
+
+const modelValue = useVModel(props, "modelValue", emits, {
+  defaultValue: props.defaultValue,
+  passive: true,
+});
+</script>

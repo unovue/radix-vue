a95f650ebed78f3141b9c81fa5fbaf4fb9c279d7
zernonia <zernonia@gmail.com>
2023-07-23 17:49:11 +0800

    add nuxt playground to demo ssr

---
+++ playground/nuxt/components/CollapsibleDemo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Icon } from "@iconify/vue";
+import { CollapsibleContent, CollapsibleRoot, CollapsibleTrigger } from "radix-vue";
+
+const rootOpen = ref(false);
+const rootDisabled = ref(false);
+</script>
+
+<template>
+  <CollapsibleRoot v-model:open="rootOpen" class="w-[300px]" :disabled="rootDisabled">
+    <div style="display: flex; align-items: center; justify-content: space-between">
+      <span class="text-white text-[15px] leading-[25px]"> @peduarte starred 3 repos </span>
+      <CollapsibleTrigger
+        class="cursor-default rounded-full h-[25px] w-[25px] inline-flex items-center justify-center text-violet11 shadow-[0_2px_10px] shadow-blackA7 outline-none data-[state=closed]:bg-white data-[state=open]:bg-violet3 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black"
+      >
+        <Icon v-if="rootOpen" icon="radix-icons:cross-2" class="h-3.5 w-3.5" />
+        <Icon v-else icon="radix-icons:row-spacing" class="h-3.5 w-3.5" />
+      </CollapsibleTrigger>
+    </div>
+
+    <div class="bg-white rounded my-[10px] p-[10px] shadow-[0_2px_10px] shadow-blackA7">
+      <span class="text-violet11 text-[15px] leading-[25px]">radix-vue/radix-vue</span>
+    </div>
+
+    <CollapsibleContent>
+      <div class="bg-white rounded my-[10px] p-[10px] shadow-[0_2px_10px] shadow-blackA7">
+        <span class="text-violet11 text-[15px] leading-[25px]">vuejs/core</span>
+      </div>
+      <div class="bg-white rounded my-[10px] p-[10px] shadow-[0_2px_10px] shadow-blackA7">
+        <span class="text-violet11 text-[15px] leading-[25px]">@radix-ui/primitives</span>
+      </div>
+    </CollapsibleContent>
+  </CollapsibleRoot>
+</template>

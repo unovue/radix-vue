36b0df42f17c297124852e88d5e36dbf9d36e9b3
zernonia <zernonia@gmail.com>
2023-07-24 17:50:59 +0800

    cleanup presence

--- packages/radix-vue/src/Presence/Presence.vue
+++ packages/radix-vue/src/Presence/Presence.vue
@@ -8,7 +8,6 @@ import {
   type VNode,
 } from "vue";
 import { usePresence } from "./usePresence";
-import { syncRef } from "@vueuse/core";
 
 interface PresenceProps {
   present: boolean;
@@ -19,27 +18,22 @@ const props = defineProps<PresenceProps>();
 const { present, forceMount } = toRefs(props);
 
 const slots = useSlots();
-const isLocalPresence = ref(props.forceMount || props.present);
 const node = ref<HTMLElement>();
 const { isPresent } = usePresence(present, node);
 
 const vPresence: Directive = {
   created(el) {
-    syncRef(isLocalPresence, isPresent, { direction: "rtl" });
     node.value = el;
   },
 };
 
 const render = () =>
-  forceMount.value || present.value || isLocalPresence.value
-    ? withDirectives(
-        slots.default?.({ present: isLocalPresence })?.[0] as VNode,
-        [[vPresence]]
-      )
+  forceMount.value || present.value || isPresent.value
+    ? withDirectives(slots.default?.()?.[0] as VNode, [[vPresence]])
     : null;
 
 defineExpose({
-  present: isLocalPresence,
+  present: isPresent,
 });
 </script>
 

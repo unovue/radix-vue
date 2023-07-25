c75a7b9a16650b9df576702d60fb6b95cdd61828
maximo <maximo@nimiq.com>
2023-07-25 10:32:10 +0700

    [Tooltip] feat: Use flg disable on hover

--- packages/radix-vue/src/Tooltip/TooltipTrigger.vue
+++ packages/radix-vue/src/Tooltip/TooltipTrigger.vue
@@ -23,6 +23,7 @@ useHover(triggerElement, {
   onHoverEnter: () => injectedValue?.showTooltip(true),
   delayLeave: injectedValue?.delayMs,
   onHoverLeave: () => injectedValue?.hideTooltip(),
+  disabled: injectedValue?.disableHoverableContent,
 });
 </script>
 
@@ -30,7 +31,7 @@ useHover(triggerElement, {
   <PopperAnchor ref="triggerElement" :as-child="props.asChild">
     <PrimitiveButton type="button" :aria-describedby="injectedValue?.open ? injectedValue.contentId : undefined
       " :as-child="props.asChild" :data-state="injectedValue?.dataState.value"
-      :aria-expanded="injectedValue?.open.value || false" @focus="injectedValue?.showTooltip"
+      :aria-expanded="injectedValue?.open.value || false" @focus="injectedValue?.showTooltip(false)"
       @blur="injectedValue?.hideTooltip" style="cursor: default">
       <slot />
     </PrimitiveButton>

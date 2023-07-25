7a2f413123d0feb7e76406ad72e01ca136b6822f
zernonia <zernonia@gmail.com>
2023-07-25 15:16:27 +0800

    add presence stories

---
+++ packages/radix-vue/src/Presence/story/PresenceBasic.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Presence } from "../";
+
+const open = ref(false);
+</script>
+
+<template>
+  <Story
+    title="Presence/Basic"
+    :layout="{ type: 'grid', width: '33%' }"
+    auto-props-disabled
+  >
+    <Variant title="Basic">
+      <div>
+        <button @click="open = !open">toggle</button>
+      </div>
+      <Presence :present="open">
+        <div>Content</div>
+      </Presence>
+    </Variant>
+  </Story>
+</template>
---
+++ packages/radix-vue/src/Presence/story/_Animation.story.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Presence from "../Presence.vue";
+import Toggle from "./_Toggle.story.vue";
+
+const open = ref(false);
+const node = ref<HTMLElement>();
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
+
+<template>
+  <Toggle :node="node" v-model:open="open"></Toggle>
+  <Presence :present="open">
+    <div v-bind="$attrs" :data-state="open ? 'open' : 'closed'" ref="node">
+      Content
+    </div>
+  </Presence>
+</template>

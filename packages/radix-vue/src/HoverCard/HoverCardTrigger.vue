4ffffe49b7f44636b3006a5380862bbbc174992f
maximo <maximo@nimiq.com>
2023-07-25 21:01:41 +0700

    Revert HoverCard changes

--- packages/radix-vue/src/HoverCard/HoverCardTrigger.vue
+++ packages/radix-vue/src/HoverCard/HoverCardTrigger.vue
@@ -3,33 +3,51 @@ export interface HoverCardTriggerProps extends PrimitiveProps { }
 </script>
 
 <script setup lang="ts">
-import { PopperAnchor } from "@/Popper";
-import { PrimitiveButton, type PrimitiveProps } from "@/Primitive";
-import { useHover } from "@/shared";
-import { inject, ref } from "vue";
+import { inject } from "vue";
+import {
+  PrimitiveButton,
+  usePrimitiveElement,
+  type PrimitiveProps,
+} from "@/Primitive";
 import {
   HOVER_CARD_INJECTION_KEY,
   type HoverCardProvideValue,
 } from "./HoverCardRoot.vue";
+import { PopperAnchor } from "@/Popper";
+
+import { useHoverDelay, useMouseleaveDelay } from "../shared";
 
 const injectedValue = inject<HoverCardProvideValue>(HOVER_CARD_INJECTION_KEY);
 
 const props = defineProps<HoverCardTriggerProps>();
-const triggerElement = ref<HTMLElement>();
 
-const isHovered = useHover(triggerElement, {
-  delayEnter: injectedValue?.openDelay,
-  onHoverEnter: () => injectedValue?.showTooltip(),
-  delayLeave: injectedValue?.closeDelay,
-  onHoverLeave: () => injectedValue?.hideTooltip(),
-});
+const { primitiveElement, currentElement } = usePrimitiveElement();
+
+async function handleMouseEnter(e: MouseEvent) {
+  const result = await useHoverDelay(
+    e,
+    currentElement.value!,
+    injectedValue?.openDelay
+  );
+  if (result) {
+    injectedValue?.showTooltip();
+  }
+}
+
+async function handleMouseleave(e: MouseEvent) {
+  injectedValue!.isHover = false;
+  const result = await useMouseleaveDelay(e, injectedValue?.closeDelay);
+  if (result && !injectedValue?.isHover) {
+    injectedValue?.hideTooltip();
+  }
+}
 </script>
 
 <template>
   <PopperAnchor asChild>
     <PrimitiveButton ref="primitiveElement" :as-child="props.asChild" :aria-expanded="injectedValue?.open.value || false"
       :data-state="injectedValue?.open.value ? 'open' : 'closed'" @mouseover="injectedValue!.isHover = true"
-      style="cursor: default">
+      @mouseenter="handleMouseEnter" @mouseleave="handleMouseleave" style="cursor: default">
       <slot />
     </PrimitiveButton>
   </PopperAnchor>

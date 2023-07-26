d556bcb427fb7e35d32b00118adb4c6e9066137f
zernonia <zernonia@gmail.com>
2023-07-27 00:57:44 +0800

    add keyframes for nav menu

--- docs/components/demo/NavigationMenu/tailwind.config.js
+++ docs/components/demo/NavigationMenu/tailwind.config.js
@@ -11,6 +11,30 @@ module.exports = {
         ...grass,
       },
     },
+    keyframes: {
+      enterFromRight: {
+        from: { opacity: 0, transform: "translateX(200px)" },
+        to: { opacity: 1, transform: "translateX(0)" },
+      },
+      enterFromLeft: {
+        from: { opacity: 0, transform: "translateX(-200px)" },
+        to: { opacity: 1, transform: "translateX(0)" },
+      },
+      exitToRight: {
+        from: { opacity: 1, transform: "translateX(0)" },
+        to: { opacity: 0, transform: "translateX(200px)" },
+      },
+      exitToLeft: {
+        from: { opacity: 1, transform: "translateX(0)" },
+        to: { opacity: 0, transform: "translateX(-200px)" },
+      },
+    },
+    animation: {
+      enterFromLeft: "enterFromLeft 250ms ease",
+      enterFromRight: "enterFromRight 250ms ease",
+      exitToLeft: "exitToLeft 250ms ease",
+      exitToRight: "exitToRight 250ms ease",
+    },
   },
   plugins: [],
 };

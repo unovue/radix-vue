1da6b0011677ebe29dc0deef5a52980f8570965b
zernonia <zernonia@gmail.com>
2023-07-27 01:06:12 +0800

    add tailwind for nav menu

--- docs/components/demo/NavigationMenu/tailwind.config.js
+++ docs/components/demo/NavigationMenu/tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require("tailwindcss/plugin");
 const { blackA, green, grass } = require("@radix-ui/colors");
 
 /** @type {import('tailwindcss').Config} */
@@ -28,13 +29,41 @@ module.exports = {
         from: { opacity: 1, transform: "translateX(0)" },
         to: { opacity: 0, transform: "translateX(-200px)" },
       },
-    },
-    animation: {
-      enterFromLeft: "enterFromLeft 250ms ease",
-      enterFromRight: "enterFromRight 250ms ease",
-      exitToLeft: "exitToLeft 250ms ease",
-      exitToRight: "exitToRight 250ms ease",
+      scaleIn: {
+        from: { opacity: 0, transform: "rotateX(-10deg) scale(0.9)" },
+        to: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+      },
+      scaleOut: {
+        from: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+        to: { opacity: 0, transform: "rotateX(-10deg) scale(0.95)" },
+      },
+      fadeIn: {
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+      },
+      fadeOut: {
+        from: { opacity: 1 },
+        to: { opacity: 0 },
+      },
     },
   },
-  plugins: [],
+  animation: {
+    scaleIn: "scaleIn 200ms ease",
+    scaleOut: "scaleOut 200ms ease",
+    fadeIn: "fadeIn 200ms ease",
+    fadeOut: "fadeOut 200ms ease",
+    enterFromLeft: "enterFromLeft 250ms ease",
+    enterFromRight: "enterFromRight 250ms ease",
+    exitToLeft: "exitToLeft 250ms ease",
+    exitToRight: "exitToRight 250ms ease",
+  },
+  plugins: [
+    plugin(({ matchUtilities }) => {
+      matchUtilities({
+        perspective: (value) => ({
+          perspective: value,
+        }),
+      });
+    }),
+  ],
 };

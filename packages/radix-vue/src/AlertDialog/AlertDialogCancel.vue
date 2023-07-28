24124e7f43b6dc04b01be4946cefa2ad3a70d8b2
Artem Melnyk <51422045+MellKam@users.noreply.github.com>
2023-07-28 21:37:18 +0200

    [AlertDialog] fix possible errors with watchEffect, implement radix-ui accesibility attirbutes that depends on element id ... (#268)
    * [AlertDialog] fix possible errors in watchEffect, add aria-labelle
    dby and aria-describedby for alert title and description, change role to alertdialog ...
    
    * move onCleanup to top of the watchEffect, just in case

--- packages/radix-vue/src/AlertDialog/AlertDialogCancel.vue
+++ packages/radix-vue/src/AlertDialog/AlertDialogCancel.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
 import { inject } from "vue";
 import { PrimitiveButton, type PrimitiveProps } from "@/Primitive";
-import {
-  ALERT_DIALOG_INJECTION_KEY,
-  type AlertDialogProvideValue,
-} from "./AlertDialogRoot.vue";
+import { ALERT_DIALOG_INJECTION_KEY } from "./AlertDialogRoot.vue";
 
 export interface AlertDialogCancelProps extends PrimitiveProps {}
 const props = defineProps<AlertDialogCancelProps>();
 
-const injectedValue = inject<AlertDialogProvideValue>(
-  ALERT_DIALOG_INJECTION_KEY
-);
-
-function handleClick() {
-  injectedValue?.closeModal();
-}
+const injectedValue = inject(ALERT_DIALOG_INJECTION_KEY);
 </script>
 
 <template>
-  <PrimitiveButton v-bind="props" type="button" @click="handleClick">
+  <PrimitiveButton
+    v-bind="props"
+    type="button"
+    @click="injectedValue?.closeModal()"
+  >
     <slot />
   </PrimitiveButton>
 </template>
--- packages/radix-vue/src/AlertDialog/AlertDialogTitle.vue
+++ packages/radix-vue/src/AlertDialog/AlertDialogTitle.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { inject } from "vue";
 import { PrimitiveH2, type PrimitiveProps } from "@/Primitive";
+import { ALERT_DIALOG_INJECTION_KEY } from "./AlertDialogRoot.vue";
 
 export interface AlertDialogTitleProps extends PrimitiveProps {}
 const props = defineProps<AlertDialogTitleProps>();
+
+const injectedValue = inject(ALERT_DIALOG_INJECTION_KEY);
 </script>
 
 <template>
-  <PrimitiveH2 v-bind="props"><slot /></PrimitiveH2>
+  <PrimitiveH2 v-bind="props" :id="injectedValue?.titleId">
+    <slot />
+  </PrimitiveH2>
 </template>

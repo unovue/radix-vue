787552548cd1d41177fe9fba07d324fdef6bc18b
zernonia <zernonia@gmail.com>
2023-08-09 23:37:37 +0800

    revert playground

--- packages/radix-vue/src/DropdownMenu/DropdownMenu.story.vue
+++ packages/radix-vue/src/DropdownMenu/DropdownMenu.story.vue
@@ -36,7 +36,7 @@ const handleCheck = (ev: any) => {
 </script>
 
 <template>
-  <Story title="DropdownMenu" :layout="{ type: 'grid' }">
+  <Story title="DropdownMenu" :layout="{ type: 'single', iframe: false }">
     <Variant title="default">
       <DropdownMenuRoot v-model:open="toggleState">
         <DropdownMenuTrigger
@@ -48,10 +48,7 @@ const handleCheck = (ev: any) => {
 
         <DropdownMenuPortal>
           <DropdownMenuContent
-            :loop="true"
             class="min-w-[220px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-            :side-offset="5"
-            :align="'start'"
           >
             <DropdownMenuItem
               value="New Tab"

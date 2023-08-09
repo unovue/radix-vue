7a3ff50dec491d186fd9ccafb511fce70dbedd3e
Unai Mengual <git@owln.ai>
2023-08-10 00:24:09 +0200

    fix(playground/nuxt): set correct dep name

--- playground/nuxt/nuxt.config.ts
+++ playground/nuxt/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["radix-nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["radix-vue/nuxt", "@nuxtjs/tailwindcss"],
 });

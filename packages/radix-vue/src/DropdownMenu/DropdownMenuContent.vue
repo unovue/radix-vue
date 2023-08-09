1aa404f0efc3d36f95bd92e236fd94e1af9e647d
zernonia <zernonia@gmail.com>
2023-08-09 23:35:47 +0800

    fix handleCloseAutoFocus not focusing on trigger element when click outside

--- packages/radix-vue/src/DropdownMenu/DropdownMenuContent.vue
+++ packages/radix-vue/src/DropdownMenu/DropdownMenuContent.vue
@@ -19,6 +19,20 @@ const context = inject(DROPDOWN_MENU_INJECTION_KEY);
 const hasInteractedOutsideRef = ref(false);
 
 const emitsAsProps = useEmitAsProps(emits);
+
+const handleCloseAutoFocus = (event: Event) => {
+  emits("closeAutoFocus", event);
+  if (event.defaultPrevented) return;
+  if (!hasInteractedOutsideRef.value) {
+    setTimeout(() => {
+      context?.triggerElement.value?.focus();
+    }, 0);
+  }
+  hasInteractedOutsideRef.value = false;
+
+  // Always prevent auto focus because we either focus manually or want user agent focus
+  event.preventDefault();
+};
 </script>
 
 <template>
@@ -26,21 +40,10 @@ const emitsAsProps = useEmitAsProps(emits);
     v-bind="{ ...props, ...emitsAsProps }"
     :id="context?.contentId"
     :aria-labelledby="context?.triggerId"
-    @close-auto-focus="
-      (event) => {
-        emits('closeAutoFocus', event);
-        if (event.defaultPrevented) return;
-        if (!hasInteractedOutsideRef) context?.triggerElement.value?.focus();
-        hasInteractedOutsideRef = false;
-
-        // Always prevent auto focus because we either focus manually or want user agent focus
-        event.preventDefault();
-      }
-    "
+    @close-auto-focus="handleCloseAutoFocus"
     @interact-outside="(event) => {
       emits('interactOutside', event)
       if(event.defaultPrevented)  return
-      
       const originalEvent = event.detail.originalEvent as PointerEvent;
       const ctrlLeftClick = originalEvent.button === 0 && originalEvent.ctrlKey === true;
       const isRightClick = originalEvent.button === 2 || ctrlLeftClick;

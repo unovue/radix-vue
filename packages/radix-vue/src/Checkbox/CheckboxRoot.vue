76c7ad8714da0815627408317a582f1af64fdb32
Dennis Smuda <smuda.dennis@gmail.com>
2023-08-12 19:47:27 +0200

    init branch

--- packages/radix-vue/src/Checkbox/CheckboxRoot.vue
+++ packages/radix-vue/src/Checkbox/CheckboxRoot.vue
@@ -2,10 +2,21 @@
 import type { InjectionKey, Ref } from "vue";
 
 export interface CheckboxRootProps extends PrimitiveProps {
+  /**
+   * The checked state of the checkbox when it is initially rendered. Use when you do not need to control its open state.
+   */
   defaultChecked?: boolean;
+
+  /**
+   * The controlled checked state of the checkbox.
+   */
   checked?: boolean;
   onCheckedChange?: void;
   modelValue?: boolean;
+
+  /**
+   * Disables the checkbox.
+   */
   disabled?: boolean;
   required?: boolean;
   name?: string;
@@ -50,29 +61,11 @@ let dataState: "checked" | "unchecked" | "indeterminate";
 </script>
 
 <template>
-  <Primitive
-    :as-child="props.asChild"
-    :as="as"
-    :value="props.value"
-    role="checkbox"
-    :aria-checked="props.modelValue"
-    :data-state="dataState"
-    style="position: relative"
-    :data-disabled="props.disabled ? '' : undefined"
-  >
-    <input
-      type="checkbox"
-      :id="props.id"
-      v-bind="props.modelValue"
-      @change="updateModelValue"
-      :checked="props.modelValue"
-      :name="props.name"
-      aria-hidden="true"
-      :disabled="props.disabled"
-      :required="props.required"
-      :data-state="dataState"
-      style="opacity: 0; position: absolute; inset: 0"
-    />
+  <Primitive :as-child="props.asChild" :as="as" :value="props.value" role="checkbox" :aria-checked="props.modelValue"
+    :data-state="dataState" style="position: relative" :data-disabled="props.disabled ? '' : undefined">
+    <input type="checkbox" :id="props.id" v-bind="props.modelValue" @change="updateModelValue" :checked="props.modelValue"
+      :name="props.name" aria-hidden="true" :disabled="props.disabled" :required="props.required" :data-state="dataState"
+      style="opacity: 0; position: absolute; inset: 0" />
     <slot />
   </Primitive>
 </template>

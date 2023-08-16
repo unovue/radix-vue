cefcd666d3cdcadbdfb0c20a662c472e0a597df6
zernonia <zernonia@gmail.com>
2023-08-16 16:20:24 +0800

    cleanup

--- packages/radix-vue/src/Select/utils.ts
+++ packages/radix-vue/src/Select/utils.ts
@@ -1,12 +1,3 @@
-import { type InjectionKey, type Ref } from "vue";
-
-type SelectItemContextValue = {
-  modelValue: Ref<boolean>;
-};
-
-export const SELECT_ITEM_SYMBOL =
-  Symbol() as InjectionKey<SelectItemContextValue>;
-
 export const OPEN_KEYS = [" ", "Enter", "ArrowUp", "ArrowDown"];
 export const SELECTION_KEYS = [" ", "Enter"];
 export const CONTENT_MARGIN = 10;

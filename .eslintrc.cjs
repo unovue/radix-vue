9c92a2d13fe48b85dbba24c8ecfe649ca90f730f
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2023-08-18 08:02:18 +0545

    style: add eslint with antfu

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+const process = require('node:process');
+
+process.env.ESLINT_TSCONFIG = 'tsconfig.json';
+
+module.exports = {
+
+  extends: ['@antfu'],
+
+  rules: {
+    'semi': ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always'],
+    'vue/no-multiple-template-root': 'off',
+    'vue/component-options-name-casing': ['error', 'kebab-case'],
+  },
+};

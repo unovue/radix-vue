1148fd8e9ec39926b9d3b6e373290742b9e8962c
zernonia <zernonia@gmail.com>
2023-08-18 12:36:37 +0800

    chore: avatar

--- packages/radix-vue/src/Avatar/AvatarImage.vue
+++ packages/radix-vue/src/Avatar/AvatarImage.vue
@@ -1,27 +1,28 @@
 <script lang="ts">
-export interface AvatarImageProps extends PrimitiveProps {
-  onLoadingStatusChange?: void;
+export interface AvatarImageProps extends PrimitiveProps {}
+export interface AvatarImageEmits {
+  (e: "loadingStatusChange", value: ImageLoadingStatus): void;
 }
 </script>
 
 <script setup lang="ts">
 import { inject, useAttrs, watch } from "vue";
-import {
-  AVATAR_INJECTION_KEY,
-  type AvatarProvideValue,
-} from "./AvatarRoot.vue";
+import { AVATAR_INJECTION_KEY } from "./AvatarRoot.vue";
 import { Primitive, type PrimitiveProps } from "../Primitive";
-import { useImageLoadingStatus } from "./utils";
+import { useImageLoadingStatus, type ImageLoadingStatus } from "./utils";
 
-const injectedValue = inject<AvatarProvideValue>(AVATAR_INJECTION_KEY);
+const injectedValue = inject(AVATAR_INJECTION_KEY);
 
 const props = withDefaults(defineProps<AvatarImageProps>(), { as: "img" });
+const emits = defineEmits<AvatarImageEmits>();
+
 const src = useAttrs().src as string;
 const imageLoadingStatus = useImageLoadingStatus(src);
 
 watch(
   imageLoadingStatus,
   (newValue) => {
+    emits("loadingStatusChange", newValue);
     if (newValue !== "idle") injectedValue!.imageLoadingStatus.value = newValue;
   },
   { immediate: true }

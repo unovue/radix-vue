8898dc3ad47395de7d5a6d4d63e3674d0be85056
zernonia <zernonia@gmail.com>
2023-08-20 00:27:37 +0800

    revert: chore: organize codebase (#313)
    This reverts commit 87ca501c6bcb7dc35c337cac6be24fb1e0c77235.

---
+++ packages/radix-vue/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/multi-word-component-names": 0,
+    "vue/no-setup-props-destructure": 1,
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+  },
+};
---
+++ packages/radix-vue/README.md
@@ -0,0 +1,16 @@
+# Radix Vue
+
+## Why can't vue have nice things?
+
+An unofficial vue port of Radix Primitives.
+An accessible, unstyled and customisable UI Library for building top quality design systems.
+
+## Installation
+
+```bash
+pnpm install radix-vue
+```
+
+## Documentation
+
+For full documentation, visit [radix-vue.com](https://radix-vue.com).

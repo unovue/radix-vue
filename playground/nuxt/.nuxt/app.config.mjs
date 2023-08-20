425dcdca666189e7e2b142f8b90fc997bc7fea1f
zernonia <zernonia@gmail.com>
2023-08-21 01:23:32 +0800

    cleanup

--- playground/nuxt/.nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/zernonia/Desktop/Radix Vue/radix-vue/playground/nuxt/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)

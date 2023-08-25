e7ea5e912f800892755ae3b328181459f5b129e6
Artem Melnyk <melkam464@gmail.com>
2023-08-25 16:51:17 +0200

    fix: misspess

--- packages/radix-vue/src/shared/createContext.ts
+++ packages/radix-vue/src/shared/createContext.ts
@@ -1,22 +1,22 @@
 import { type InjectionKey, inject, provide } from 'vue'
 
 export function createContext<ContextValue>(rootComponentName: string) {
-  const injectionKey: InjectionKey<ContextValue> = Symbol(
+  const injectionKey: InjectionKey<ContextValue | null> = Symbol(
     `${rootComponentName}Context`,
   )
 
   /**
-   * @throws When failed to inject context and fallback not providee.
+   * @throws When failed to inject context and fallback not specified.
    * This happens when the component injecting the context is not a child of the root component providing the context.
    */
   const injectContext = <T extends ContextValue | null | undefined = ContextValue>(
     fallback?: T,
   ): T extends null ? ContextValue | null : ContextValue => {
-    const context = inject(injectionKey, fallback || undefined)
+    const context = inject(injectionKey, fallback)
     if (context)
       return context
 
-    if (fallback === null)
+    if (context === null)
       return null as any
 
     throw new Error(`Component must be used within ${rootComponentName}`)

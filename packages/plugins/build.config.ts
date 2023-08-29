66a5e25b2b4e46280197cffdc26e9350e0f057eb
zernonia <zernonia@gmail.com>
2023-08-29 22:38:50 +0800

    refactor: change from vite to unbuild for plugins

---
+++ packages/plugins/build.config.ts
@@ -0,0 +1,30 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig([
+  {
+    name: 'Nuxt module',
+    entries: ['./src/nuxt/index'],
+    outDir: '../radix-vue/dist',
+    clean: false,
+    declaration: true,
+    externals: [
+      '@nuxt/schema',
+    ],
+    rollup: {
+      emitCJS: true,
+    },
+  },
+  {
+    name: 'Unplugin-vue-component Resolver',
+    entries: ['./src/resolver/index'],
+    outDir: '../radix-vue/dist',
+    clean: false,
+    declaration: true,
+    externals: [
+      'unplugin-vue-components',
+    ],
+    rollup: {
+      emitCJS: true,
+    },
+  },
+])
--- packages/radix-nuxt/build.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineBuildConfig } from 'unbuild'
-
-export default defineBuildConfig({
-  failOnWarn: false,
-  outDir: '../radix-vue/dist/nuxt',
-  externals: [
-    '@nuxt/schema',
-  ],
-})

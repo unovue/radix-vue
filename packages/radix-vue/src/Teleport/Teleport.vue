c957b28824718f14bed58d9d7cca9aa209e0363d
zernonia <zernonia@gmail.com>
2023-09-08 16:59:04 +0800

    feat: create teleport primitive that would handle client side teleport

---
+++ packages/radix-vue/src/Teleport/Teleport.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+export interface TeleportProps {
+  to?: string | HTMLElement
+  disabled?: boolean
+  forceMount?: boolean
+}
+
+withDefaults(defineProps<TeleportProps>(), {
+  to: 'body',
+})
+
+import { useMounted } from '@vueuse/core'
+
+const isMounted = useMounted()
+</script>
+
+<template>
+  <Teleport v-if="isMounted || forceMount" :to="to" :disabled="disabled">
+    <slot />
+  </Teleport>
+</template>

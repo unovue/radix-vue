8723a5ac35b54ec860e92f6ac69f29f4c8d7548f
zernonia <zernonia@gmail.com>
2023-09-09 23:09:41 +0800

    test: alert dialog

--- packages/radix-vue/src/AlertDialog/story/_AlertDialog.vue
+++ packages/radix-vue/src/AlertDialog/story/_AlertDialog.vue
@@ -20,7 +20,7 @@ const isOpen = ref(false)
     <AlertDialogTrigger
       class="text-violet11 hover:bg-mauve3 shadow-blackA7 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] outline-none focus:shadow-[0_0_0_2px] focus:shadow-black"
     >
-      Delete account
+      Open
     </AlertDialogTrigger>
     <AlertDialogPortal>
       <AlertDialogOverlay
@@ -30,7 +30,7 @@ const isOpen = ref(false)
         class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
       >
         <AlertDialogTitle class="text-mauve12 m-0 text-[17px] font-medium">
-          Are you absolutely sure?
+          Title
         </AlertDialogTitle>
         <AlertDialogDescription
           class="text-mauve11 mt-4 mb-5 text-[15px] leading-normal"

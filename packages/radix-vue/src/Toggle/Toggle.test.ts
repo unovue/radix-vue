07959ec5a16ac37eae2b255a00287fe02213cac7
zernonia <zernonia@gmail.com>
2023-09-11 22:41:28 +0800

    test: toggle

--- packages/radix-vue/src/Toggle/Toggle.test.ts
+++ packages/radix-vue/src/Toggle/Toggle.test.ts
@@ -1,11 +1,43 @@
-import { expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 import { Toggle } from './'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 
-it('should pass axe accessibility tests', async () => {
-  const wrapper = mount(Toggle, {
-    attrs: { 'aria-label': 'Toggle italic' },
+describe('given default Toggle', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Toggle>>
+
+  beforeEach(() => {
+    wrapper = mount(Toggle, {
+      attrs: { 'aria-label': 'Toggle italic' },
+    })
   })
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  it('should not be toggled yet', () => {
+    expect(wrapper.attributes('data-state')).toBe('off')
+  })
+
+  describe('after toggling', () => {
+    beforeEach(() => {
+      wrapper.trigger('click')
+    })
+
+    it('should be toggled on', () => {
+      expect(wrapper.attributes('data-state')).toBe('on')
+    })
+
+    describe('after toggling again', () => {
+      beforeEach(() => {
+        wrapper.trigger('click')
+      })
+
+      it('should be toggled off', () => {
+        expect(wrapper.attributes('data-state')).toBe('off')
+      })
+    })
+  })
 })

f04fcb3b07b405c814bf5e2818d6bb35f5bab5c7
zernonia <zernonia@gmail.com>
2023-09-11 21:54:27 +0800

    test: slider

--- packages/radix-vue/src/Slider/Slider.test.ts
+++ packages/radix-vue/src/Slider/Slider.test.ts
@@ -1,15 +1,52 @@
-import { expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { axe } from 'vitest-axe'
 import Slider from './story/_Slider.vue'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 
-it('should pass axe accessibility tests', async () => {
+describe('given default Slider', () => {
   globalThis.ResizeObserver = class ResizeObserver {
     observe() {}
     unobserve() {}
     disconnect() {}
   }
+  window.HTMLElement.prototype.scrollIntoView = vi.fn()
+  window.HTMLElement.prototype.hasPointerCapture = vi.fn()
+  window.HTMLElement.prototype.releasePointerCapture = vi.fn()
+  window.HTMLElement.prototype.setPointerCapture = vi.fn()
 
-  const wrapper = mount(Slider)
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+  let wrapper: VueWrapper<InstanceType<typeof Slider>>
+
+  beforeEach(() => {
+    wrapper = mount(Slider)
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    wrapper = mount(Slider)
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  it('should have defalt value', () => {
+    expect(wrapper.html()).toContain('aria-valuenow="50"')
+  })
+
+  // TODO: Properly test dragging event
+  // describe('after dragging the handler', () => {
+  //   beforeEach(async () => {
+  //     const trigger = wrapper.find('[role=slider]')
+  //     await fireEvent.pointerDown(trigger.element)
+  //     await fireEvent.pointerMove(trigger.element, {
+  //       x: 10,
+  //     })
+
+  //     // await trigger.trigger('pointerdown')
+  //     // await trigger.trigger('pointermove', {
+  //     //   x: 10,
+  //     // })
+  //   })
+
+  //   it('should do something', () => {
+  //     console.log('hi', wrapper.html())
+  //   })
+  // })
 })

3ba6d04000319ab0cb1e39cb0fd95a9d91b47f2f
zernonia <zernonia@gmail.com>
2023-09-12 00:35:59 +0800

    test: tooltip

--- packages/radix-vue/src/Tooltip/Tooltip.test.ts
+++ packages/radix-vue/src/Tooltip/Tooltip.test.ts
@@ -1,17 +1,42 @@
-import { expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 import Tooltip from './stories/_Tooltip.vue'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 
-it('should pass axe accessibility tests', async () => {
+describe('given default Tooltip', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Tooltip>>
+
   globalThis.ResizeObserver = class ResizeObserver {
     observe() {}
     unobserve() {}
     disconnect() {}
   }
-  const wrapper = mount(Tooltip, { attachTo: document.body })
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+  beforeEach(() => {
+    document.body.innerHTML = ''
+    wrapper = mount(Tooltip, { attachTo: document.body })
+  },
+  )
 
-  await wrapper.find('button').trigger('focus')
-  expect(await axe(document.body)).toHaveNoViolations()
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+
+    await wrapper.find('button').trigger('focus')
+    expect(await axe(document.body)).toHaveNoViolations()
+  })
+
+  it('should open when focus', async () => {
+    await wrapper.find('button').trigger('focus')
+    expect(document.body.innerHTML).toContain('Add to library')
+  })
+
+  describe('after focusing out', () => {
+    beforeEach(() => {
+      document.body.focus()
+    })
+
+    it('should close the tooltip', () => {
+      expect(document.body.innerHTML).not.toContain('Add to library')
+    })
+  })
 })

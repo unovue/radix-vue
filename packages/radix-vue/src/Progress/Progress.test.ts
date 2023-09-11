ef42272ae433abf92700447a9b834d11ca8ca0b3
zernonia <zernonia@gmail.com>
2023-09-11 13:39:26 +0800

    test: progress

--- packages/radix-vue/src/Progress/Progress.test.ts
+++ packages/radix-vue/src/Progress/Progress.test.ts
@@ -1,9 +1,25 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 import Progress from './story/_Progress.vue'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 
-it('should pass axe accessibility tests', async () => {
-  const wrapper = mount(Progress)
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+describe('given a default Progress', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Progress>>
+
+  it('should pass axe accessibility tests', async () => {
+    wrapper = mount(Progress)
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  it('shoud contain correct value', () => {
+    expect(wrapper.html()).toContain('data-value="0"')
+  })
+
+  describe('after 2 seconds', () => {
+    it('shoud contain correct value', async () => {
+      await new Promise(resolve => setTimeout(resolve, 200))
+      expect(wrapper.html()).toContain('data-value="50"')
+    })
+  })
 })

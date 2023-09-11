ac686085206d2cc40e108fd7f334504a96ad6877
zernonia <zernonia@gmail.com>
2023-09-11 22:38:15 +0800

    test: tabs

--- packages/radix-vue/src/Tabs/Tabs.test.ts
+++ packages/radix-vue/src/Tabs/Tabs.test.ts
@@ -1,9 +1,40 @@
-import { expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 import Tabs from './story/_Tabs.vue'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 
-it('should pass axe accessibility tests', async () => {
-  const wrapper = mount(Tabs)
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+describe('given default Tabs', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Tabs>>
+
+  beforeEach(() => {
+    wrapper = mount(Tabs, { attachTo: document.body })
+    wrapper.find('button').element.focus()
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  it('should render tab\'s content', () => {
+    expect(wrapper.find('[role=tabpanel]').exists()).toBeTruthy()
+    expect(wrapper.html()).toContain('Make changes')
+  })
+
+  describe('after changing tab', () => {
+    beforeEach(() => {
+      const trigger = wrapper.find('button')
+      trigger.trigger('keydown', { key: 'ArrowRight' })
+    })
+
+    it('should focus on next tab', () => {
+      const trigger = wrapper.findAll('button')[1]
+      expect(trigger.element).toBe(document.activeElement)
+    })
+
+    it('should render it\'s content', () => {
+      expect(wrapper.find('[role=tabpanel]').exists()).toBeTruthy()
+      expect(wrapper.html()).toContain('Change your password')
+    })
+  })
 })

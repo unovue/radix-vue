9516ad71587ea5a7482ff14c7b405a8af735d1e6
zernonia <zernonia@gmail.com>
2023-09-12 00:18:58 +0800

    test: toolbar

--- packages/radix-vue/src/Toolbar/Toolbar.test.ts
+++ packages/radix-vue/src/Toolbar/Toolbar.test.ts
@@ -1,9 +1,26 @@
-import { expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import Toolbar from './story/_Toolbar.vue'
+import type { DOMWrapper, VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 import { axe } from 'vitest-axe'
 
-it('should pass axe accessibility tests', async () => {
-  const wrapper = mount(Toolbar)
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+describe('given default Toolbar', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Toolbar>>
+  let triggers: DOMWrapper<HTMLElement>[]
+
+  beforeEach(() => {
+    wrapper = mount(Toolbar, { attachTo: document.body })
+    triggers = wrapper.findAll('button')
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  it('should have default selected value', () => {
+    const selected = triggers.filter(i => i.attributes('data-state') === 'on').map(i => i.element)
+    expect(selected.includes(triggers[4].element)).toBeTruthy()
+  })
+
+  // Since Toolbar is just a collection of ToggleGroup, so would exclude the test here
 })

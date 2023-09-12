5e523e0945f9c95e657cb3684d80df9a1120681a
zernonia <zernonia@gmail.com>
2023-09-12 10:26:18 +0800

    test: menu

--- packages/radix-vue/src/Menu/Menu.test.ts
+++ packages/radix-vue/src/Menu/Menu.test.ts
@@ -1,16 +1,62 @@
-import { expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 import { axe } from 'vitest-axe'
 import Menu from './story/_Menu.vue'
+import { findAllByRole } from '@testing-library/vue'
+import { nextTick } from 'vue'
 
-it('should pass axe accessibility tests', async () => {
+describe('given a default Menu', () => {
   globalThis.ResizeObserver = class ResizeObserver {
     observe() {}
     unobserve() {}
     disconnect() {}
   }
-  mount(Menu, {
-    attachTo: document.body,
+  let wrapper: VueWrapper<InstanceType<typeof Menu>>
+  beforeEach(() => {
+    document.body.innerHTML = ''
+    wrapper = mount(Menu, {
+      attachTo: document.body,
+    })
   })
-  expect(await axe(document.body)).toHaveNoViolations()
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(document.body)).toHaveNoViolations()
+  })
+
+  it('should have all the groups', async () => {
+    const groups = await findAllByRole(document.body, 'group')
+    expect(groups.length).toBe(4)
+  })
+
+  it('should render item in group', async () => {
+    const group = (await findAllByRole(document.body, 'group'))[0]
+    const items = await findAllByRole(group, 'menuitem')
+    expect(items.length).toBe(5)
+  })
+
+  describe('after focusing on item', () => {
+    let firstItem: HTMLElement
+    beforeEach(async () => {
+      const items = await findAllByRole(document.body, 'menuitem')
+      firstItem = items[0]
+      firstItem.focus()
+      await nextTick()
+    })
+
+    it('should have hightlighted state', () => {
+      expect(firstItem.parentElement?.innerHTML).toContain('data-highlighted')
+    })
+
+    describe('after selecting the item', () => {
+      beforeEach(async () => {
+        firstItem.click()
+        await nextTick()
+      })
+
+      it('should emit select', () => {
+        expect(wrapper.emitted('select')?.length).toBe(1)
+      })
+    })
+  })
 })

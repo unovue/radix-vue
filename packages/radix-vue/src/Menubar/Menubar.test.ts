123342563fa75a4e47885ebd819e2ea17d41b1ad
zernonia <zernonia@gmail.com>
2023-09-12 11:13:25 +0800

    test: menubar

--- packages/radix-vue/src/Menubar/Menubar.test.ts
+++ packages/radix-vue/src/Menubar/Menubar.test.ts
@@ -1,15 +1,60 @@
-import { expect, it } from 'vitest'
-import { axe } from 'vitest-axe'
+import { beforeEach, describe, expect, it } from 'vitest'
 import Menubar from './story/_Menubar.vue'
+import type { VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
-import { nextTick } from 'vue'
+import { axe } from 'vitest-axe'
+import { findByRole } from '@testing-library/vue'
 
-it('should pass axe accessibility tests', async () => {
-  const wrapper = mount(Menubar, { attachTo: document.body })
-  expect(await axe(wrapper.element)).toHaveNoViolations()
+describe('given default Menubar', () => {
+  let wrapper: VueWrapper<InstanceType<typeof Menubar>>
+  globalThis.ResizeObserver = class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
 
-  // open modal
-  wrapper.find('button').element.click()
-  await nextTick()
-  expect(await axe(document.body)).toHaveNoViolations()
+  beforeEach(() => {
+    document.body.innerHTML = ''
+    wrapper = mount(Menubar, { attachTo: document.body })
+  })
+
+  it('should render all trigger button', () => {
+    expect(wrapper.findAll('button').length).toBe(4)
+  })
+
+  it('should pass axe accessibility tests', async () => {
+    expect(await axe(wrapper.element)).toHaveNoViolations()
+  })
+
+  describe('after opening the dropdown', () => {
+    beforeEach(async () => {
+      await wrapper.find('button').trigger('pointerdown', {
+        button: 0,
+        ctrlKey: false,
+      })
+    })
+
+    it('should pass axe accessibility tests', async () => {
+      expect(await axe(wrapper.element)).toHaveNoViolations()
+    })
+
+    it('should render the menu', async () => {
+      expect(await findByRole(wrapper.element as HTMLElement, 'menu')).toBeTruthy()
+    })
+
+    describe('after selecting the first item', () => {
+      beforeEach(async () => {
+        const item = wrapper.find('[role="menu"]').find('[role="menuitem"]')
+        await item.trigger('click')
+      })
+
+      it('should close the modal', () => {
+        expect(wrapper.find('[role="menu"]').exists()).toBeFalsy()
+      })
+
+      it('should emit select event', () => {
+        expect(wrapper.emitted('select')?.length).toBe(1)
+      })
+    })
+  })
 })

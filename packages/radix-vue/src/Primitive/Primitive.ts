cf90feca5835cf0b8e45ff86418960b9dfb63fc3
zernonia <59365435+zernonia@users.noreply.github.com>
2023-09-26 14:47:36 +0800

    feat: expose Slot, Primitive components (#417)

--- packages/radix-vue/src/Primitive/Primitive.ts
+++ packages/radix-vue/src/Primitive/Primitive.ts
@@ -1,5 +1,5 @@
-import { type PropType, cloneVNode, defineComponent, h, mergeProps } from 'vue'
-import { isValidVNodeElement, renderSlotFragments } from '@/shared'
+import { type PropType, defineComponent, h } from 'vue'
+import { Slot } from './Slot'
 
 export type AsTag =
   | 'a'
@@ -21,23 +21,6 @@ export type AsTag =
   | 'template'
   // eslint-disable-next-line @typescript-eslint/ban-types
   | ({} & string) // any other string
-
-function throwError(componentName: string) {
-  throw new Error(
-    [
-      `Detected an invalid children for \`${componentName}\` with \`asChild\` prop.`,
-      '',
-      'Note: All components accepting `asChild` expect only one direct child of valid VNode type.',
-      'You can apply a few solutions:',
-      [
-        'Provide a single child element so that we can forward the props onto that element.',
-        'Ensure the first child is an actual element instead of a raw text node or comment node.',
-      ]
-        .map(line => `  - ${line}`)
-        .join('\n'),
-    ].join('\n'),
-  )
-}
 
 export interface PrimitiveProps {
   /**
@@ -70,38 +53,6 @@ export const Primitive = defineComponent({
     if (asTag !== 'template')
       return () => h(props.as, attrs, { default: slots.default })
 
-    return () => {
-      if (!slots.default)
-        return null
-      const childrens = renderSlotFragments(slots.default())
-
-      const [firstChildren, ...otherChildren] = childrens
-      if (!isValidVNodeElement(firstChildren))
-        throwError(asTag)
-
-      if (Object.keys(attrs).length > 0) {
-        // remove props ref from being inferred
-        delete firstChildren.props?.ref
-        const mergedProps = mergeProps(attrs, firstChildren.props ?? {})
-        // remove class to prevent duplicated
-        if (attrs.class && firstChildren.props?.class)
-          delete firstChildren.props.class
-
-        const cloned = cloneVNode(firstChildren, mergedProps)
-        // Explicitly override props starting with `on`.
-        // It seems cloneVNode from Vue doesn't like overriding `onXXX` props. So
-        // we have to do it manually.
-        for (const prop in mergedProps) {
-          if (prop.startsWith('on')) {
-            cloned.props ||= {}
-            cloned.props[prop] = mergedProps[prop]
-          }
-        }
-
-        return childrens.length === 1 ? cloned : [cloned, ...otherChildren]
-      }
-
-      return childrens
-    }
+    return () => h(Slot, attrs, { default: slots.default })
   },
 })

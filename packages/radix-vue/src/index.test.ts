95d08c1048dccb8f7553a356d9f075098afb3d94
zernonia <zernonia@gmail.com>
2023-09-28 14:15:07 +0800

    fix: test

--- packages/radix-vue/src/index.test.ts
+++ packages/radix-vue/src/index.test.ts
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest'
 import * as Radix from './index'
-import { components } from '../constant'
+import { components, utilities } from '../constant'
 
 it('should expose the correct components', () => {
-  expect(Object.keys(Radix)).toEqual(Object.values(components).flat())
+  expect(Object.keys(Radix)).toEqual([...Object.values(components).flat(), ...Object.values(utilities).flat()])
 })

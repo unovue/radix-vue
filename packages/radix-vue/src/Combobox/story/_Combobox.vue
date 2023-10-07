9b2a70eeb75e4149bc4ba236ebfcbe2b54d6618a
zernonia <zernonia@gmail.com>
2023-10-07 16:41:35 +0800

    test: update test based on update useVModel

--- packages/radix-vue/src/Combobox/story/_Combobox.vue
+++ packages/radix-vue/src/Combobox/story/_Combobox.vue
@@ -7,10 +7,12 @@ const props = defineProps<ComboboxRootProps>()
 const v = ref('')
 const options = ['Apple', 'Banana', 'Blueberry', 'Grapes', 'Pineapple']
 const vegetables = ['Aubergine', 'Broccoli', 'Carrot', 'Courgette', 'Leek']
+
+const open = ref(props.open)
 </script>
 
 <template>
-  <ComboboxRoot v-bind="props" v-model="v" name="test">
+  <ComboboxRoot v-bind="props" v-model="v" v-model:open="open" name="test">
     <ComboboxHeader class="min-w-[160px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-grass11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-grass9 outline-none">
       <ComboboxInput class="bg-transparent outline-none text-grass11 placeholder-gray-400" placeholder="Placeholder..." />
       <ComboboxTrigger>

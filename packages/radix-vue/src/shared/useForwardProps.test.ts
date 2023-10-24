71511059091e72ad346016191b5fbb90f0f6d30f
zernonia <zernonia@gmail.com>
2023-10-24 12:30:17 +0800

    test: add useForwardProp test

--- packages/radix-vue/src/shared/useForwardProps.test.ts
+++ packages/radix-vue/src/shared/useForwardProps.test.ts
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import { defineComponent, watch } from 'vue'
 import { useForwardProps } from './useForwardProps'
+import { reactivePick } from '@vueuse/shared'
 
 function setupTestComponent(props: Record<string, any>) {
   return defineComponent({
@@ -43,5 +44,24 @@ describe('useForwardProps', () => {
   it('should not forwarded not provided props', () => {
     const wrapper = mount(setupTestComponent({ id: { type: String, default: 'test' } }), { props: { extra: 'not-related' }, attrs: { class: 'custom' } })
     expect(wrapper.emitted('log')?.[0][0]).toStrictEqual({ id: 'test' })
+  })
+
+  it('should not forwarded props that is not passed as parameters', async () => {
+    const component = defineComponent({
+      props: { id: { type: String, default: 'test' }, extra: { type: String, default: 'not-related' } },
+      emits: ['log'],
+      setup(props, { emit }) {
+        const picked = reactivePick(props, 'id')
+        const forwarded = useForwardProps(picked)
+        watch(forwarded, () => emit('log', forwarded.value), { immediate: true, deep: true })
+
+        return { forwarded }
+      },
+      template: '<div>{{ forwarded }}</div>',
+    })
+    const wrapper = mount(component)
+    expect(wrapper.emitted('log')?.[0][0]).toStrictEqual({ id: 'test' })
+    await wrapper.setProps({ id: 'new-test' })
+    expect(wrapper.emitted('log')?.[1][0]).toStrictEqual({ id: 'new-test' })
   })
 })

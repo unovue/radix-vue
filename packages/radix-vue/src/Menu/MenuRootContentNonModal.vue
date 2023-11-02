ef9f56696d5131d23592cf2f0bef4c219f6d37ef
zernonia <zernonia@gmail.com>
2023-11-02 11:18:30 +0800

    fix: missing forwarded in MenuContent

--- packages/radix-vue/src/Menu/MenuRootContentNonModal.vue
+++ packages/radix-vue/src/Menu/MenuRootContentNonModal.vue
@@ -4,23 +4,21 @@ import MenuContentImpl, {
   type MenuRootContentProps,
 } from './MenuContentImpl.vue'
 import { injectMenuContext } from './MenuRoot.vue'
-import { useEmitAsProps } from '@/shared'
+import { useForwardPropsEmits } from '@/shared'
 
 const props = defineProps<MenuRootContentNonModalProps>()
-
 const emits = defineEmits<MenuRootContentModalEmits>()
+const forwarded = useForwardPropsEmits(props, emits)
 
 const menuContext = injectMenuContext()
 
 interface MenuRootContentNonModalProps extends MenuRootContentProps {}
 type MenuRootContentModalEmits = MenuContentImplEmits
-
-const emitsAsProps = useEmitAsProps(emits)
 </script>
 
 <template>
   <MenuContentImpl
-    v-bind="{ ...props, ...emitsAsProps }"
+    v-bind="forwarded"
     :trap-focus="false"
     :disable-outside-pointer-events="false"
     :disable-outside-scroll="false"

0cebdc8c938c4e7c1022c9cb83f34a174faaeca3
zernonia <zernonia@gmail.com>
2023-11-03 19:36:03 +0800

    refactor: dialog overlay to allow stacking with useBodyScrollLock

---
+++ packages/radix-vue/src/Dialog/DialogOverlayImpl.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import type { PrimitiveProps } from '@/Primitive'
+
+export interface DialogOverlayImplProps extends PrimitiveProps {}
+</script>
+
+<script setup lang="ts">
+import { injectDialogRootContext } from './DialogRoot.vue'
+import { Primitive } from '@/Primitive'
+import { useBodyScrollLock } from '@/shared'
+
+defineProps<DialogOverlayImplProps>()
+const rootContext = injectDialogRootContext()
+
+useBodyScrollLock(true)
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :data-state="rootContext.open.value ? 'open' : 'closed'"
+    style="pointer-events: auto"
+    data-aria-hidden="true"
+    aria-hidden="true"
+  >
+    <slot />
+  </Primitive>
+</template>

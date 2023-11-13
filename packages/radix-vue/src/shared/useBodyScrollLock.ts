56e4acbe92d296c91fa76b51c4aa29c89361a57b
zernonia <zernonia@gmail.com>
2023-11-13 18:58:12 +0800

    refactor: useBodyScrollLock

--- packages/radix-vue/src/shared/useBodyScrollLock.ts
+++ packages/radix-vue/src/shared/useBodyScrollLock.ts
@@ -1,13 +1,12 @@
 import {
   createGlobalState,
-  defaultDocument,
-  useScrollLock,
 } from '@vueuse/core'
 import { isClient } from '@vueuse/shared'
-import { computed, nextTick, onBeforeUnmount, ref } from 'vue'
+import { nextTick, onBeforeUnmount, ref, watch } from 'vue'
 import { defu } from 'defu'
 import { injectConfigProviderContext } from '@/ConfigProvider/ConfigProvider.vue'
 
+const useInitialOverflowStyle = createGlobalState(() => ref<string | undefined>())
 const useBodyLockStackCount = createGlobalState(() => ref(0))
 
 export function useBodyScrollLock(initialState?: boolean | undefined) {
@@ -16,69 +15,69 @@ export function useBodyScrollLock(initialState?: boolean | undefined) {
   })
 
   const stack = useBodyLockStackCount()
-  const locked = useScrollLock(defaultDocument?.body, false)
+  const initialOverflow = useInitialOverflowStyle()
 
-  const writableLock = computed<boolean>({
-    get() {
-      return locked.value
-    },
-    set(newLocked) {
-      if (!isClient)
-        return
+  const locked = ref(initialState)
 
-      if (newLocked) {
-        const verticalScrollbarWidth
-        = window.innerWidth - document.documentElement.clientWidth
-
-        const defaultConfig = { padding: verticalScrollbarWidth, margin: 0 }
-
-        const config = context.scrollBody?.value
-          ? typeof context.scrollBody.value === 'object'
-            ? defu({
-              padding: context.scrollBody.value.padding === true ? verticalScrollbarWidth : context.scrollBody.value.padding,
-              margin: context.scrollBody.value.margin === true ? verticalScrollbarWidth : context.scrollBody.value.margin,
-            }, defaultConfig)
-            : defaultConfig
-          : ({ padding: 0, margin: 0 })
-
-        if (verticalScrollbarWidth > 0) {
-          document.body.style.paddingRight = `${config.padding}px`
-          document.body.style.marginRight = `${config.margin}px`
-          document.body.style.setProperty('--scrollbar-width', `${verticalScrollbarWidth}px`)
-        }
-
-        // let dismissibleLayer set previous pointerEvent first
-        nextTick(() => {
-          document.body.style.pointerEvents = 'none'
-          locked.value = true
-        })
-      }
-      else {
-        document.body.style.paddingRight = ''
-        document.body.style.marginRight = ''
-        document.body.style.pointerEvents = ''
-        document.body.style.removeProperty('--scrollbar-width')
-        locked.value = false
-      }
-    },
-  })
-
-  if (initialState) {
-    stack.value++
-    writableLock.value = initialState
+  const resetBodyStyle = () => {
+    document.body.style.paddingRight = ''
+    document.body.style.marginRight = ''
+    document.body.style.pointerEvents = ''
+    document.body.style.removeProperty('--scrollbar-width')
+    document.body.style.overflow = initialOverflow.value ?? ''
+    initialOverflow.value = undefined
   }
+
+  if (initialState)
+    stack.value++
+
+  watch(locked, (val) => {
+    if (!isClient)
+      return
+
+    if (val) {
+      if (!initialOverflow.value)
+        initialOverflow.value = document.body.style.overflow
+
+      const verticalScrollbarWidth
+    = window.innerWidth - document.documentElement.clientWidth
+
+      const defaultConfig = { padding: verticalScrollbarWidth, margin: 0 }
+
+      const config = context.scrollBody?.value
+        ? typeof context.scrollBody.value === 'object'
+          ? defu({
+            padding: context.scrollBody.value.padding === true ? verticalScrollbarWidth : context.scrollBody.value.padding,
+            margin: context.scrollBody.value.margin === true ? verticalScrollbarWidth : context.scrollBody.value.margin,
+          }, defaultConfig)
+          : defaultConfig
+        : ({ padding: 0, margin: 0 })
+
+      if (verticalScrollbarWidth > 0) {
+        document.body.style.paddingRight = `${config.padding}px`
+        document.body.style.marginRight = `${config.margin}px`
+        document.body.style.setProperty('--scrollbar-width', `${verticalScrollbarWidth}px`)
+        document.body.style.overflow = 'hidden'
+      }
+
+      // let dismissibleLayer set previous pointerEvent first
+      nextTick(() => {
+        document.body.style.pointerEvents = 'none'
+      })
+    }
+    else {
+      resetBodyStyle()
+    }
+  }, { immediate: true })
 
   onBeforeUnmount(() => {
     if (!initialState)
       return
+
     stack.value--
-    if (stack.value === 0) {
-      document.body.style.paddingRight = ''
-      document.body.style.marginRight = ''
-      document.body.style.pointerEvents = ''
-      document.body.style.removeProperty('--scrollbar-width')
-    }
+    if (stack.value === 0)
+      resetBodyStyle()
   })
 
-  return writableLock
+  return locked
 }

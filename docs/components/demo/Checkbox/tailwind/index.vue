3d0b0a1e57a505051891bcd0c5a433c7258d3e15
zernonia <zernonia@gmail.com>
2023-11-15 08:48:24 +0800

    docs: fix checkbox example

--- docs/components/demo/Checkbox/tailwind/index.vue
+++ docs/components/demo/Checkbox/tailwind/index.vue
@@ -10,7 +10,7 @@ const checkboxOne = ref(true)
   <div class="flex flex-col gap-2.5">
     <label class="flex flex-row gap-4 items-center [&>.checkbox]:hover:bg-neutral-100">
       <CheckboxRoot
-        v-model="checkboxOne"
+        v-model:checked="checkboxOne"
         class="shadow-blackA7 hover:bg-green3 flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-[4px] bg-white shadow-[0_2px_10px] outline-none focus-within:shadow-[0_0_0_2px_black]"
       >
         <CheckboxIndicator class="bg-white h-full w-full rounded flex items-center justify-center">

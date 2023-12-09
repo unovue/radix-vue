efef818a8f9faea0f85ea27473360a7fb1970263
zernonia <zernonia@gmail.com>
2023-12-09 23:21:32 +0800

    docs: update props table styling

--- docs/components/tables/PropsTable.vue
+++ docs/components/tables/PropsTable.vue
@@ -25,65 +25,44 @@ const props = defineProps<PropsTableProps>()
     </div>
     <thead class="">
       <ProseTr class="">
-        <ProseTh class="w-[45%]">
+        <ProseTh class="w-1/6">
           <span>Prop</span>
         </ProseTh>
-        <ProseTh>
-          <span>Type</span>
-        </ProseTh>
-        <ProseTh>
+        <ProseTh class="w-1/6">
           <span>Default</span>
+        </ProseTh>
+        <ProseTh class="w-2/3">
+          <span>Type</span>
         </ProseTh>
       </ProseTr>
     </thead>
+
     <tbody>
       <ProseTr v-for="(prop, index) in props.data" :key="`${prop.name}-${index}`">
         <ProseTd>
-          <div class="flex items-center gap-1">
+          <div class="flex h-full items-start gap-1">
             <ProseCodeInline class="!text-[13px]">
               {{ prop.name }}{{ prop.required ? "*" : null }}
             </ProseCodeInline>
-            <template v-if="prop.description">
+            <!-- <template v-if="prop.description">
               <PopoverRoot>
                 <PopoverTrigger class="p-1.5 rounded hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-2 outline-neutral-800">
                   <Icon icon="radix-icons:info-circled" class="h-4 w-4 text-neutral-400" />
                 </PopoverTrigger>
                 <PopoverContent
                   side="top"
-                  class="text-xs rounded-lg px-3 py-2.5 bg-white dark:bg-neutral-800 w-[265px] shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                  class="vp-raw text-xs rounded-lg px-3 py-2.5 bg-white dark:bg-neutral-800 w-[265px] shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
                 >
                   <span size="2" class="leading-5" v-html="prop.description" />
                   <PopoverArrow class="dark:fill-neutral-800 fill-white" :size="8" />
                 </PopoverContent>
               </PopoverRoot>
-            </template>
+            </template> -->
           </div>
         </ProseTd>
+
         <ProseTd>
-          <div class="flex items-center gap-1">
-            <ProseCodeInline variant="secondary">
-              {{ prop.typeSimple ? prop.typeSimple : prop.type }}
-            </ProseCodeInline>
-            <template v-if="prop.typeSimple">
-              <PopoverRoot>
-                <PopoverTrigger class="p-1.5 rounded hover:bg-neutral-900 focus:outline-2 outline-neutral-800">
-                  <Icon icon="radix-icons:info-circled" class="h-4 w-4 text-neutral-400" />
-                </PopoverTrigger>
-                <PopoverContent
-                  side="top"
-                  class="text-xs rounded-lg px-3 py-2.5 bg-neutral-800 max-w-[265px] min-w-max shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
-                >
-                  <ProseCodeInline class="whitespace-nowrap !text-[12px]">
-                    {{ prop.type }}
-                  </ProseCodeInline>
-                  <PopoverArrow class="fill-neutral-800" :size="8" />
-                </PopoverContent>
-              </PopoverRoot>
-            </template>
-          </div>
-        </ProseTd>
-        <ProseTd>
-          <div v-if="prop.default" class="flex items-center gap-1">
+          <div v-if="prop.default" class="flex  h-full items-start gap-1">
             <ProseCodeInline variant="secondary">
               {{ prop.default }}
             </ProseCodeInline>
@@ -93,6 +72,33 @@ const props = defineProps<PropsTableProps>()
               <Icon icon="radix-icons:divider-horizontal" />
             </div>
           </template>
+        </ProseTd>
+
+        <ProseTd>
+          <div class="flex items-center gap-1">
+            <div>
+              <ProseCodeInline variant="secondary">
+                {{ prop.typeSimple ? prop.typeSimple : prop.type }}
+              </ProseCodeInline>
+              <p class="vp-raw mt-2 text-sm text-neutral-500" v-html="prop.description" />
+            </div>
+            <template v-if="prop.typeSimple">
+              <PopoverRoot>
+                <PopoverTrigger class="p-1.5 rounded hover:bg-neutral-900 focus:outline-2 outline-neutral-800">
+                  <Icon icon="radix-icons:info-circled" class="h-4 w-4 text-neutral-400" />
+                </PopoverTrigger>
+                <PopoverContent
+                  side="top"
+                  class="vp-raw text-xs rounded-lg px-3 py-2.5 bg-neutral-800 max-w-[265px] min-w-max shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                >
+                  <ProseCodeInline class="whitespace-nowrap !text-[12px]">
+                    {{ prop.type }}
+                  </ProseCodeInline>
+                  <PopoverArrow class="fill-neutral-800" :size="8" />
+                </PopoverContent>
+              </PopoverRoot>
+            </template>
+          </div>
         </ProseTd>
       </ProseTr>
     </tbody>

2c5e1a37fdce4ce9d3848b46966ef3de01d7ff26
zernonia <zernonia@gmail.com>
2024-01-31 19:00:55 +0800

    chore: fix story

--- packages/radix-vue/src/Tooltip/stories/Tooltip.content.story.vue
+++ packages/radix-vue/src/Tooltip/stories/Tooltip.content.story.vue
@@ -62,7 +62,6 @@ const lastEvent = ref('')
             </TooltipTrigger>
             <Teleport to="body">
               <TooltipContent
-                as-child
                 :side-offset="5"
                 class="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
                 @escape-key-down="lastEvent = '@on-escape-key-down'"

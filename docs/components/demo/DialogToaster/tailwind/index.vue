e8ccd1382127868f24da0ecc9b74bf945de58547
zernonia <zernonia@gmail.com>
2024-02-10 22:45:21 +0800

    Revert "fix(dialog): replace `vue-sonner` to `radix-vue` Toast Component"
    This reverts commit 819e0d2ce34e74f89a366e20cb29a3f25557b73b.

--- docs/components/demo/DialogToaster/tailwind/index.vue
+++ docs/components/demo/DialogToaster/tailwind/index.vue
@@ -8,33 +8,10 @@ import {
   DialogRoot,
   DialogTitle,
   DialogTrigger,
-  ToastAction, ToastDescription, ToastProvider, ToastRoot, ToastTitle, ToastViewport,
 } from 'radix-vue'
 import { Icon } from '@iconify/vue'
-import { ref } from 'vue'
 
-const open = ref(false)
-const eventDateRef = ref(new Date())
-const timerRef = ref(0)
-
-function oneWeekAway() {
-  const now = new Date()
-  const inOneWeek = now.setDate(now.getDate() + 7)
-  return new Date(inOneWeek)
-}
-
-function prettyDate(date: Date) {
-  return new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'short' }).format(date)
-}
-
-function handleClick() {
-  open.value = false
-  window.clearTimeout(timerRef.value)
-  timerRef.value = window.setTimeout(() => {
-    eventDateRef.value = oneWeekAway()
-    open.value = true
-  }, 100)
-}
+import { Toaster, toast } from 'vue-sonner'
 </script>
 
 <template>
@@ -49,6 +26,10 @@ function handleClick() {
         <DialogOverlay class="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 z-30" />
         <DialogContent
           class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-[100]"
+          @interact-outside="event => {
+            const target = event.target as HTMLElement;
+            if (target?.closest('[data-sonner-toaster]')) return event.preventDefault()
+          }"
         >
           <DialogTitle class="text-mauve12 m-0 text-[17px] font-semibold">
             Dialog Title
@@ -59,7 +40,12 @@ function handleClick() {
 
           <button
             class="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-semibold leading-none focus:shadow-[0_0_0_2px] focus:outline-none"
-            @click="handleClick"
+            @click="() => toast('Event has been created', {
+              action: {
+                label: 'Undo',
+                onClick: () => console.log('Undo'),
+              },
+            })"
           >
             Give me a toast
           </button>
@@ -74,31 +60,8 @@ function handleClick() {
       </DialogPortal>
     </DialogRoot>
 
-    <Teleport to="body">
-      <ToastProvider>
-        <ToastRoot
-          v-model:open="open"
-          class="bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
-        >
-          <ToastTitle class="[grid-area:_title] mb-[5px] font-medium text-slate12 text-[15px]">
-            Scheduled: Catch up
-          </ToastTitle>
-          <ToastDescription>
-            <time
-              class="[grid-area:_description] m-0 text-slate11 text-[13px] leading-[1.3]"
-              :dateTime="eventDateRef.toISOString()"
-            >
-              {{ prettyDate(eventDateRef) }}
-            </time>
-          </ToastDescription>
-          <ToastAction class="[grid-area:_action]" alt-text="Goto schedule to undo">
-            <button class="inline-flex items-center justify-center rounded font-medium text-xs px-[10px] leading-[25px] h-[25px] bg-green2 text-green11 shadow-[inset_0_0_0_1px] shadow-green7 hover:shadow-[inset_0_0_0_1px] hover:shadow-green8 focus:shadow-[0_0_0_2px] focus:shadow-green8">
-              Undo
-            </button>
-          </ToastAction>
-        </ToastRoot>
-        <ToastViewport class="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
-      </ToastProvider>
+    <Teleport to="html">
+      <Toaster />
     </Teleport>
   </div>
 </template>

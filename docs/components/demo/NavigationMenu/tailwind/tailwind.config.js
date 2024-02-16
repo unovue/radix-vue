2baaefd178c0413e4fdb5312c8c86a04dd148125
zernonia <zernonia@gmail.com>
2024-02-16 12:23:58 +0100

    docs: fix NavigationMenu tailwindconfig

--- docs/components/demo/NavigationMenu/tailwind/tailwind.config.js
+++ docs/components/demo/NavigationMenu/tailwind/tailwind.config.js
@@ -12,51 +12,51 @@ module.exports = {
         ...grass,
         ...teal,
       },
+      keyframes: {
+        enterFromRight: {
+          from: { opacity: 0, transform: 'translateX(200px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        enterFromLeft: {
+          from: { opacity: 0, transform: 'translateX(-200px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        exitToRight: {
+          from: { opacity: 1, transform: 'translateX(0)' },
+          to: { opacity: 0, transform: 'translateX(200px)' },
+        },
+        exitToLeft: {
+          from: { opacity: 1, transform: 'translateX(0)' },
+          to: { opacity: 0, transform: 'translateX(-200px)' },
+        },
+        scaleIn: {
+          from: { opacity: 0, transform: 'rotateX(-10deg) scale(0.9)' },
+          to: { opacity: 1, transform: 'rotateX(0deg) scale(1)' },
+        },
+        scaleOut: {
+          from: { opacity: 1, transform: 'rotateX(0deg) scale(1)' },
+          to: { opacity: 0, transform: 'rotateX(-10deg) scale(0.95)' },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        fadeOut: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+      },
+      animation: {
+        scaleIn: 'scaleIn 200ms ease',
+        scaleOut: 'scaleOut 200ms ease',
+        fadeIn: 'fadeIn 200ms ease',
+        fadeOut: 'fadeOut 200ms ease',
+        enterFromLeft: 'enterFromLeft 250ms ease',
+        enterFromRight: 'enterFromRight 250ms ease',
+        exitToLeft: 'exitToLeft 250ms ease',
+        exitToRight: 'exitToRight 250ms ease',
+      },
     },
-    keyframes: {
-      enterFromRight: {
-        from: { opacity: 0, transform: 'translateX(200px)' },
-        to: { opacity: 1, transform: 'translateX(0)' },
-      },
-      enterFromLeft: {
-        from: { opacity: 0, transform: 'translateX(-200px)' },
-        to: { opacity: 1, transform: 'translateX(0)' },
-      },
-      exitToRight: {
-        from: { opacity: 1, transform: 'translateX(0)' },
-        to: { opacity: 0, transform: 'translateX(200px)' },
-      },
-      exitToLeft: {
-        from: { opacity: 1, transform: 'translateX(0)' },
-        to: { opacity: 0, transform: 'translateX(-200px)' },
-      },
-      scaleIn: {
-        from: { opacity: 0, transform: 'rotateX(-10deg) scale(0.9)' },
-        to: { opacity: 1, transform: 'rotateX(0deg) scale(1)' },
-      },
-      scaleOut: {
-        from: { opacity: 1, transform: 'rotateX(0deg) scale(1)' },
-        to: { opacity: 0, transform: 'rotateX(-10deg) scale(0.95)' },
-      },
-      fadeIn: {
-        from: { opacity: 0 },
-        to: { opacity: 1 },
-      },
-      fadeOut: {
-        from: { opacity: 1 },
-        to: { opacity: 0 },
-      },
-    },
-  },
-  animation: {
-    scaleIn: 'scaleIn 200ms ease',
-    scaleOut: 'scaleOut 200ms ease',
-    fadeIn: 'fadeIn 200ms ease',
-    fadeOut: 'fadeOut 200ms ease',
-    enterFromLeft: 'enterFromLeft 250ms ease',
-    enterFromRight: 'enterFromRight 250ms ease',
-    exitToLeft: 'exitToLeft 250ms ease',
-    exitToRight: 'exitToRight 250ms ease',
   },
   plugins: [
     plugin(({ matchUtilities }) => {

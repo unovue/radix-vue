7b50c38bcdc636b6d083c45fe63323539536fbb3
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-02-21 22:34:08 +0200

    chore(DatePicker): add story styling

--- packages/radix-vue/src/DatePicker/story/DatePicker.story.vue
+++ packages/radix-vue/src/DatePicker/story/DatePicker.story.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
-import { type DateValue } from '@internationalized/date'
 import {
   DatePickerArrow,
   DatePickerCalendar,
@@ -15,6 +14,7 @@ import {
   DatePickerHeadCell,
   DatePickerHeader,
   DatePickerHeading,
+  DatePickerHeadingSegment,
   DatePickerInput,
   DatePickerNext,
   DatePickerPrev,
@@ -27,84 +27,105 @@ import { Label } from '@/Label'
 <template>
   <Story title="Date Picker">
     <Variant title="default">
-      <Label for="date-field">Input label</Label>
-      <DatePickerRoot
-        id="date-field"
-        :is-date-unavailable="(date:DateValue) => date.day === 19"
-        granularity="second"
-      >
-        <DatePickerField v-slot="{ segments }">
-          <DatePickerInput v-for="item in segments" :key="item.part" :part="item.part">
-            {{ item.value }}
-          </DatePickerInput>
-          <DatePickerTrigger>
-            <Icon icon="radix-icons:calendar" class="w-6 h-6" />
-          </DatePickerTrigger>
-        </DatePickerField>
-
-        <DatePickerContent class="rounded-lg bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.green7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade">
-          <DatePickerArrow class="fill-white" />
-          <DatePickerCalendar
-            v-slot="{ weekDays, months }"
-            class="p-[22px]"
+      <div class="flex flex-col gap-2">
+        <Label class="text-sm text-gray9" for="date-field">Birthday</Label>
+        <DatePickerRoot
+          id="date-field"
+          :is-date-unavailable="date => date.day === 19"
+          granularity="second"
+        >
+          <DatePickerField
+            v-slot="{ segments }"
+            class="flex select-none items-center rounded text-center text-green10 placeholder:text-mauve5 border border-gray9 p-2 data-[invalid]:border-red-500"
           >
-            <DatePickerHeader class="flex items-center justify-between">
-              <DatePickerPrev
-                class="inline-flex items-center cursor-pointer text-black justify-center rounded-[9px] bg-transparent w-10 h-10 hover:bg-black hover:text-white active:scale-98 active:transition-all"
+            <template v-for="item in segments" :key="item.part">
+              <DatePickerInput
+                v-if="item.part === 'literal'"
+                :part="item.part"
+                class="p-1"
               >
-                <Icon icon="radix-icons:chevron-left" class="w-6 h-6" />
-              </DatePickerPrev>
-              <DatePickerHeading class="text-[15px] text-black font-medium" />
-              <DatePickerNext
-                class="inline-flex items-center cursor-pointer justify-center text-black rounded-[9px] bg-transparent w-10 h-10 hover:bg-black hover:text-white active:scale-98 active:transition-all"
+                {{ item.value }}
+              </DatePickerInput>
+              <DatePickerInput
+                v-else
+                :part="item.part"
+                class="rounded-5px px-1 py-1 hover:bg-muted focus:bg-muted focus:text-foreground focus-visible:!ring-0 focus-visible:!ring-offset-0 aria-[valuetext=Empty]:text-muted-foreground"
               >
-                <Icon icon="radix-icons:chevron-right" class="w-6 h-6" />
-              </DatePickerNext>
-            </DatePickerHeader>
-            <div
-              class="flex flex-col space-y-4 pt-4 sm:flex-row sm:space-x-4 sm:space-y-0"
+                {{ item.value }}
+              </DatePickerInput>
+            </template>
+
+            <DatePickerTrigger>
+              <Icon icon="radix-icons:calendar" class="w-6 h-6" />
+            </DatePickerTrigger>
+          </DatePickerField>
+
+          <DatePickerContent
+            align="end"
+            :side-offset="16"
+            class="rounded-lg bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.green7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+          >
+            <DatePickerArrow class="fill-white" />
+            <DatePickerCalendar
+              v-slot="{ weekDays, grid }"
+              class="p-[22px]"
             >
-              <DatePickerGrid v-for="month in months" :key="month.value.toString()" class="w-full border-collapse select-none space-y-1">
-                <DatePickerGridHead>
-                  <DatePickerGridRow class="mb-1 flex w-full justify-between">
-                    <DatePickerHeadCell
-                      v-for="day in weekDays" :key="day"
-                      class="w-10 rounded-md text-xs !font-normal text-black"
-                    >
-                      {{ day }}
-                    </DatePickerHeadCell>
-                  </DatePickerGridRow>
-                </DatePickerGridHead>
-                <DatePickerGridBody>
-                  <DatePickerGridRow
-                    v-for="(weekDates, index) in month.weeks"
-                    :key="`weekDate-${index}`"
-                    class="flex w-full"
-                  >
-                    <DatePickerCell
-                      v-for="weekDate in weekDates"
-                      :key="weekDate.toString()"
-                      :date="weekDate"
-                      class="relative !p-0 text-center text-sm w-10 h-10"
-                    >
-                      <DatePickerCellTrigger
-                        :day="weekDate"
-                        :month="month.value"
-                        class="group relative inline-flex items-center justify-center whitespace-nowrap rounded-[9px] border border-transparent bg-transparent p-0 text-sm font-normal text-black w-10 h-10 hover:border-black data-[disabled]:pointer-events-none data-[outside-month]:pointer-events-none data-[selected]:bg-black data-[selected]:font-medium data-[disabled]:text-black/30 data-[selected]:text-white data-[unavailable]:text-black/30 data-[unavailable]:line-through"
+              <DatePickerHeader class="flex items-center justify-between">
+                <DatePickerPrev
+                  class="inline-flex items-center cursor-pointer text-black justify-center rounded-[9px] bg-transparent w-10 h-10 hover:bg-black hover:text-white active:scale-98 active:transition-all"
+                >
+                  <Icon icon="radix-icons:chevron-left" class="w-6 h-6" />
+                </DatePickerPrev>
+
+                <DatePickerHeading v-slot="{ headingValue }" class="text-[15px] text-black font-medium">
+                  <DatePickerHeadingSegment v-for="item in headingValue" :key="item.value" :type="item.type" :value="item.value" />
+                </DatePickerHeading>
+                <DatePickerNext
+                  class="inline-flex items-center cursor-pointer justify-center text-black rounded-[9px] bg-transparent w-10 h-10 hover:bg-black hover:text-white active:scale-98 active:transition-all"
+                >
+                  <Icon icon="radix-icons:chevron-right" class="w-6 h-6" />
+                </DatePickerNext>
+              </DatePickerHeader>
+              <div
+                class="flex flex-col space-y-4 pt-4 sm:flex-row sm:space-x-4 sm:space-y-0"
+              >
+                <DatePickerGrid v-for="month in grid" :key="month.value.toString()" class="w-full border-collapse select-none space-y-1">
+                  <DatePickerGridHead>
+                    <DatePickerGridRow class="mb-1 flex w-full justify-between">
+                      <DatePickerHeadCell
+                        v-for="day in weekDays" :key="day"
+                        class="w-10 rounded-md text-xs !font-normal text-black"
                       >
-                        <div
-                          class="absolute top-[5px] hidden rounded-full w-1 h-1 group-data-[today]:block group-data-[today]:bg-grass9 group-data-[selected]:bg-white"
+                        {{ day }}
+                      </DatePickerHeadCell>
+                    </DatePickerGridRow>
+                  </DatePickerGridHead>
+                  <DatePickerGridBody>
+                    <DatePickerGridRow
+                      v-for="(weekDates, index) in month.rows"
+                      :key="`weekDate-${index}`"
+                      class="flex w-full"
+                    >
+                      <DatePickerCell
+                        v-for="weekDate in weekDates"
+                        :key="weekDate.toString()"
+                        :date="weekDate"
+                        class="relative !p-0 text-center text-sm w-10 h-10"
+                      >
+                        <DatePickerCellTrigger
+                          :day="weekDate"
+                          :month="month.value"
+                          class="relative flex items-center justify-center whitespace-nowrap rounded-[9px] border border-transparent bg-transparent text-sm font-normal text-black p-2 hover:border-black data-[disabled]:pointer-events-none data-[outside-view]:pointer-events-none data-[selected]:bg-black data-[selected]:font-medium data-[disabled]:text-black/30 data-[selected]:text-white data-[unavailable]:text-black/30 data-[unavailable]:line-through before:absolute before:top-[5px] before:hidden before:rounded-full before:w-1 before:h-1 before:bg-white data-[today]:before:block data-[today]:before:bg-grass9 data-[selected]:before:bg-white"
                         />
-                        {{ weekDate.day }}
-                      </DatePickerCellTrigger>
-                    </DatePickerCell>
-                  </DatePickerGridRow>
-                </DatePickerGridBody>
-              </DatePickerGrid>
-            </div>
-          </DatePickerCalendar>
-        </DatePickerContent>
-      </DatePickerRoot>
+                      </DatePickerCell>
+                    </DatePickerGridRow>
+                  </DatePickerGridBody>
+                </DatePickerGrid>
+              </div>
+            </DatePickerCalendar>
+          </DatePickerContent>
+        </DatePickerRoot>
+      </div>
     </Variant>
   </Story>
 </template>

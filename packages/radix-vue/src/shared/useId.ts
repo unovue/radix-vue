b0a279a421c77193ab7a34b2a577deaca8c52f6c
zernonia <zernonia@gmail.com>
2024-03-04 21:33:56 +0800

    test: fix broken test

--- packages/radix-vue/src/shared/useId.ts
+++ packages/radix-vue/src/shared/useId.ts
@@ -20,5 +20,5 @@ export function useId(deterministicId?: string | null | undefined) {
     useId: () => `radix-${++count}`,
   })
 
-  return deterministicId ?? useId!()
+  return deterministicId ?? useId?.() ?? `radix-${++count}`
 }

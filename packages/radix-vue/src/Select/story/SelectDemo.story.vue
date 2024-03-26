78b5b292d60f6fe9159f0fd04ee576988a4c71d7
Slava Cherkesov <chrtyaka@gmail.com>
2024-03-26 16:59:39 +0100

    fix(select): fix select root element render
    Select content root renders fragment. Transition can't be applied to fragment. Changed to div

--- packages/radix-vue/src/Select/story/SelectDemo.story.vue
+++ packages/radix-vue/src/Select/story/SelectDemo.story.vue
@@ -45,61 +45,75 @@ const POSITION = ['item-aligned', 'popper'] as const
           </SelectTrigger>
 
           <SelectPortal>
-            <SelectContent
-              class="min-w-[160px] bg-white overflow-hidden rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-              :side-offset="5"
-              :position="position"
-            >
-              <SelectScrollUpButton
-                class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default"
+            <Transition>
+              <SelectContent
+                class="min-w-[160px] bg-white overflow-hidden rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+                :side-offset="5"
+                :position="position"
               >
-                <Icon icon="radix-icons:chevron-up" />
-              </SelectScrollUpButton>
+                <SelectScrollUpButton
+                  class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default"
+                >
+                  <Icon icon="radix-icons:chevron-up" />
+                </SelectScrollUpButton>
 
-              <SelectViewport class="p-[5px]">
-                <SelectLabel
-                  class="px-[25px] text-xs leading-[25px] text-mauve11"
-                >
-                  Fruits
-                </SelectLabel>
-                <SelectGroup>
-                  <SelectItemWrapper :options="options" />
-                </SelectGroup>
-                <SelectSeparator class="h-[1px] bg-violet6 m-[5px]" />
-                <SelectLabel
-                  class="px-[25px] text-xs leading-[25px] text-mauve11"
-                >
-                  Vegetables
-                </SelectLabel>
-                <SelectGroup>
-                  <SelectItem
-                    v-for="(option, index) in vegetables"
-                    :key="index"
-                    class="text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
-                    :value="option"
-                    :disabled="option === 'Courgette'"
+                <SelectViewport class="p-[5px]">
+                  <SelectLabel
+                    class="px-[25px] text-xs leading-[25px] text-mauve11"
                   >
-                    <SelectItemIndicator
-                      class="absolute left-0 w-[25px] inline-flex items-center justify-center"
+                    Fruits
+                  </SelectLabel>
+                  <SelectGroup>
+                    <SelectItemWrapper :options="options" />
+                  </SelectGroup>
+                  <SelectSeparator class="h-[1px] bg-violet6 m-[5px]" />
+                  <SelectLabel
+                    class="px-[25px] text-xs leading-[25px] text-mauve11"
+                  >
+                    Vegetables
+                  </SelectLabel>
+                  <SelectGroup>
+                    <SelectItem
+                      v-for="(option, index) in vegetables"
+                      :key="index"
+                      class="text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+                      :value="option"
+                      :disabled="option === 'Courgette'"
                     >
-                      <Icon icon="radix-icons:check" />
-                    </SelectItemIndicator>
-                    <SelectItemText>
-                      {{ option }}
-                    </SelectItemText>
-                  </SelectItem>
-                </SelectGroup>
-              </SelectViewport>
+                      <SelectItemIndicator
+                        class="absolute left-0 w-[25px] inline-flex items-center justify-center"
+                      >
+                        <Icon icon="radix-icons:check" />
+                      </SelectItemIndicator>
+                      <SelectItemText>
+                        {{ option }}
+                      </SelectItemText>
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectViewport>
 
-              <SelectScrollDownButton
-                class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default"
-              >
-                <Icon icon="radix-icons:chevron-down" />
-              </SelectScrollDownButton>
-            </SelectContent>
+                <SelectScrollDownButton
+                  class="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default"
+                >
+                  <Icon icon="radix-icons:chevron-down" />
+                </SelectScrollDownButton>
+              </SelectContent>
+            </Transition>
           </SelectPortal>
         </SelectRoot>
       </div>
     </Variant>
   </Story>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity .3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>

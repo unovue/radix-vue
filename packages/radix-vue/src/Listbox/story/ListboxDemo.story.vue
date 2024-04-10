a3b1b119bb1a94ba51d65697cf1b9b4dbc8c5667
zernonia <zernonia@gmail.com>
2024-04-10 20:56:25 +0800

    chore: update story

--- packages/radix-vue/src/Listbox/story/ListboxDemo.story.vue
+++ packages/radix-vue/src/Listbox/story/ListboxDemo.story.vue
@@ -15,9 +15,9 @@ const filteredOptions = computed(() => {
     title="Listbox/Demo"
     :layout="{ type: 'single', iframe: false }"
   >
-    <ListboxRoot class="w-48 h-72 flex flex-col p-1 rounded-lg border bg-white text-green9 mx-auto overflow-auto">
-      <ListboxContent>
-        <ListboxItem v-for="i in filteredOptions.map(i => i.label).slice(0, 50)" :key="i" :value="i" class="w-full py-1 px-2 text-green9 select-none text-sm focus:ring-0 data-[highlighted]:outline-green9 data-[highlighted]:outline-1 data-[highlighted]:outline focus:outline-green9 data-[state=checked]:bg-green9 data-[state=checked]:text-white data-[disabled]:opacity-50 rounded">
+    <ListboxRoot class="w-48 h-72 flex flex-col rounded-lg border bg-white text-green9 mx-auto overflow-auto">
+      <ListboxContent class="p-1">
+        <ListboxItem v-for="i in filteredOptions.map(i => i.label).slice(0, 50)" :key="i" :value="i" class="w-full py-1 px-2 text-green9 select-none text-sm outline-none data-[highlighted]:ring-green9 data-[highlighted]:ring-1 focus:ring-green9 focus:ring-1  data-[state=checked]:bg-green9 data-[state=checked]:text-white data-[disabled]:opacity-50 rounded">
           {{ i }}
         </ListboxItem>
       </ListboxContent>

ce512bb67c12bd668cecb641073d4289f34ab999
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-04-20 22:47:21 +0300

    docs(Calendar): add Label to CalendarSelect story

--- packages/radix-vue/src/Calendar/story/CalendarSelect.story.vue
+++ packages/radix-vue/src/Calendar/story/CalendarSelect.story.vue
@@ -2,6 +2,7 @@
 import { Icon } from '@iconify/vue'
 import { CalendarCell, CalendarCellTrigger, CalendarGrid, CalendarGridBody, CalendarGridHead, CalendarGridRow, CalendarHeadCell, CalendarHeader, CalendarHeading, CalendarNext, CalendarPrev, CalendarRoot } from '../'
 import { SelectContent, SelectGroup, SelectItem, SelectItemIndicator, SelectItemText, SelectLabel, SelectPortal, SelectRoot, SelectScrollDownButton, SelectScrollUpButton, SelectSeparator, SelectTrigger, SelectValue, SelectViewport } from '@/Select'
+import { Label } from '@/Label'
 import { computed, ref } from 'vue'
 import { createCalendar, getLocalTimeZone, toCalendar, today } from '@internationalized/date'
 
@@ -54,6 +55,7 @@ const value = computed(() => toCalendar(today(getLocalTimeZone()), createCalenda
   <Story title="Calendar/Locale and Calendar Select" :layout="{ type: 'single' }">
     <Variant title="default">
       <div class="flex flex-col gap-4">
+        <Label class="text-mauve11">Locale</Label>
         <SelectRoot v-model="locale" @update:model-value="updateLocale">
           <SelectTrigger
             class="min-w-[160px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
@@ -103,6 +105,7 @@ const value = computed(() => toCalendar(today(getLocalTimeZone()), createCalenda
           </SelectPortal>
         </SelectRoot>
 
+        <Label class="text-mauve11">Calendar</Label>
         <SelectRoot v-model="calendar">
           <SelectTrigger
             class="min-w-[160px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"

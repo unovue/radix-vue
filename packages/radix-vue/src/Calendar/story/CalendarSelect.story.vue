c98e4e4915caf4a6c9f65ebd25e56f8bad730c4e
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-04-20 22:06:06 +0300

    docs: use v-model in CalendarSelect

--- packages/radix-vue/src/Calendar/story/CalendarSelect.story.vue
+++ packages/radix-vue/src/Calendar/story/CalendarSelect.story.vue
@@ -54,7 +54,7 @@ const value = computed(() => toCalendar(today(getLocalTimeZone()), createCalenda
   <Story title="Calendar/Locale and Calendar Select" :layout="{ type: 'single' }">
     <Variant title="default">
       <div class="flex flex-col gap-4">
-        <SelectRoot :model-value="locale" @update:model-value="updateLocale">
+        <SelectRoot v-model="locale" @update:model-value="updateLocale">
           <SelectTrigger
             class="min-w-[160px] inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
             aria-label="Customise options"

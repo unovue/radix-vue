ac6bec18dc9fdb2966b4cc27f6ace171a9db0d87
dwightjack <marco.solazzi@gmail.com>
2024-05-01 18:10:41 +0900

    docs(accordion): fix aria-hidden attribute in examples

--- docs/components/demo/Accordion/tailwind/index.vue
+++ docs/components/demo/Accordion/tailwind/index.vue
@@ -36,7 +36,6 @@ const accordionItems = [
             <Icon
               icon="radix-icons:chevron-down"
               class="text-green10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
-              aria-hidden
             />
           </AccordionTrigger>
         </AccordionHeader>

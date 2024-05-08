483ff3910f8d9be90d51504837b7d223e8f1f3ea
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-05-09 00:11:37 +0300

    docs(Editable): implement stories

--- packages/radix-vue/src/Editable/story/EditableDefault.story.vue
+++ packages/radix-vue/src/Editable/story/EditableDefault.story.vue
@@ -1,17 +1,26 @@
 <script lang="ts" setup>
-import { EditableCancelTrigger, EditableEditTrigger, EditableInput, EditablePreview, EditableRoot, EditableSubmitTrigger } from '../'
+import { EditableArea, EditableCancelTrigger, EditableEditTrigger, EditableInput, EditablePreview, EditableRoot, EditableSubmitTrigger } from '../'
 </script>
 
 <template>
   <Story title="Editable/Default" :layout="{ type: 'single' }">
     <Variant title="default">
-      <EditableRoot v-slot="{ isEditing }" data-testid="root">
-        <EditablePreview data-testid="preview" />
-        <EditableInput data-testid="input" />
-        <EditableEditTrigger v-if="!isEditing" data-testid="edit" />
-        <div v-else class="flex flex-col gap-4">
-          <EditableSubmitTrigger data-testid="submit" />
-          <EditableCancelTrigger data-testid="cancel" />
+      <EditableRoot v-slot="{ isEditing }" placeholder="Enter text..." class="flex flex-col gap-4">
+        <EditableArea class="text-green10">
+          <EditablePreview />
+          <EditableInput />
+        </EditableArea>
+        <EditableEditTrigger
+          v-if="!isEditing"
+          class="inline-flex items-center justify-center rounded font-medium text-[15px] px-[15px] leading-[35px] h-[35px] bg-white text-violet11 shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black"
+        />
+        <div v-else class="flex gap-4">
+          <EditableSubmitTrigger
+            class="inline-flex items-center justify-center rounded font-medium text-[15px] px-[15px] leading-[35px] h-[35px] bg-grass9 text-white shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-grass8 focus:shadow-[0_0_0_2px] focus:shadow-black"
+          />
+          <EditableCancelTrigger
+            class="inline-flex items-center justify-center rounded font-medium text-[15px] px-[15px] leading-[35px] h-[35px] bg-white text-violet11 shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black"
+          />
         </div>
       </EditableRoot>
     </Variant>

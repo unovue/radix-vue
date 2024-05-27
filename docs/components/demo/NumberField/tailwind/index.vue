6e2adf311f76572cd11219d41837186cabec28a9
zernonia <zernonia@gmail.com>
2024-05-27 23:03:22 +0800

    docs: add back label

--- docs/components/demo/NumberField/tailwind/index.vue
+++ docs/components/demo/NumberField/tailwind/index.vue
@@ -5,10 +5,12 @@ import { Icon } from '@iconify/vue'
 
 <template>
   <NumberFieldRoot
+    id="age"
     class="text-sm text-white"
     :min="0"
     :default-value="18"
   >
+    <label for="age">Age</label>
     <div class="mt-1 flex items-center border bg-blackA7 border-blackA9 rounded-md">
       <NumberFieldDecrement class="p-2 disabled:opacity-20">
         <Icon icon="radix-icons:minus" />

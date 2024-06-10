6cf10d177307eb84086397f17d2f110d58e20a3f
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-10 16:16:51 +0300

    feat(Stepper): add Separator component

--- packages/radix-vue/src/Stepper/StepperSeparator.vue
+++ packages/radix-vue/src/Stepper/StepperSeparator.vue
@@ -1,3 +1,21 @@
+<script lang="ts">
+import { Separator, type SeparatorProps } from '@/Separator'
+import { useForwardExpose } from '@/shared'
+
+export interface StepperSeparatorProps extends SeparatorProps {}
+</script>
+
+<script setup lang="ts">
+import { injectStepperRootContext } from './StepperRoot.vue'
+
+const props = withDefaults(defineProps<StepperSeparatorProps>(), {  })
+
+useForwardExpose()
+const rootContext = injectStepperRootContext()
+</script>
+
 <template>
-  <p>StepperSeparator</p>
+  <Separator v-bind="props" decorative :orientation="rootContext.orientation.value">
+    <slot />
+  </Separator>
 </template>

cfaaa21f74ee3859826a22fcd42b69b252595dac
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-10 16:16:21 +0300

    feat(Stepper): add Indicator component

--- packages/radix-vue/src/Stepper/StepperIndicator.vue
+++ packages/radix-vue/src/Stepper/StepperIndicator.vue
@@ -1,3 +1,28 @@
+<script lang="ts">
+import type { PrimitiveProps } from '@/Primitive'
+import { injectStepperRootContext } from './StepperRoot.vue'
+import { useForwardExpose } from '@/shared'
+import { injectStepperItemContext } from './StepperItem.vue';
+
+export interface StepperIndicatorProps extends PrimitiveProps {}
+</script>
+
+<script setup lang="ts">
+import { Primitive } from '@/Primitive'
+
+const props = defineProps<StepperIndicatorProps>()
+const rootContext = injectStepperRootContext()
+const itemContext = injectStepperItemContext()
+useForwardExpose()
+</script>
+
 <template>
-<p>StepperIndicator</p>
+  <Primitive
+    v-bind="props"
+    :data-orientation="rootContext.orientation.value"
+    :data-state="itemContext.state.value"
+    :data-disabled="itemContext.disabled.value"
+  >
+    <slot />
+  </Primitive>
 </template>

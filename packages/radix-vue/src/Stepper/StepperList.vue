dd9152eec9dd272b3fa3499711d45a0638183b07
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-10 16:16:00 +0300

    feat(Stepper): add List component

---
+++ packages/radix-vue/src/Stepper/StepperList.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import type { PrimitiveProps } from '@/Primitive'
+import { useForwardExpose } from '@/shared'
+
+export interface StepperListProps extends PrimitiveProps {}
+</script>
+
+<script setup lang="ts">
+import { injectStepperRootContext } from './StepperRoot.vue'
+import { Primitive } from '@/Primitive'
+
+const rootContext = injectStepperRootContext()
+const props = withDefaults(defineProps<StepperListProps>(), { as: rootContext.linear.value ? 'ol' : 'ul' })
+
+useForwardExpose()
+</script>
+
+<template>
+  <Primitive v-bind="props" :data-orientation="rootContext.orientation.value" :data-linear="rootContext.linear.value ? '' : undefined">
+    <slot />
+  </Primitive>
+</template>

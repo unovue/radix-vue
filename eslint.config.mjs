a438310dae5203d9da059a00e92a9ac37726a804
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-14 00:25:54 +0300

    chore: update eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    vue: true,
+    typescript: true,
+  },
+  {
     ignores: ['*.js'],
   },
   {
@@ -9,8 +13,6 @@ export default antfu(
       'ts/no-non-null-asserted-optional-chain': 'off',
       'ts/ban-ts-comment': 'warn',
       'ts/consistent-type-definitions': 'off',
-      'import/first': 'off',
-      'import/order': 'off',
       'symbol-description': 'off',
       'no-console': 'warn',
       'max-statements-per-line': ['error', { max: 2 }],
@@ -18,6 +20,15 @@ export default antfu(
       'unicorn/prefer-dom-node-text-content': 'off',
       'unicorn/prefer-number-properties': 'off',
       'regexp/no-super-linear-backtracking': 'off',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 1,
+        multiline: 1,
+      }],
     },
   },
   {

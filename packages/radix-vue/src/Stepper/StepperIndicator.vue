aa9ba77f385b433734c368a08fe4203f5a81d124
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-18 00:25:25 +0300

    refactor(Stepper): remove data attributes

--- packages/radix-vue/src/Stepper/StepperIndicator.vue
+++ packages/radix-vue/src/Stepper/StepperIndicator.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-import { injectStepperRootContext } from './StepperRoot.vue'
 import { injectStepperItemContext } from './StepperItem.vue'
 import type { PrimitiveProps } from '@/Primitive'
 import { useForwardExpose } from '@/shared'
@@ -11,7 +10,6 @@ import { Primitive } from '@/Primitive'
 export interface StepperIndicatorProps extends PrimitiveProps { }
 
 const props = defineProps<StepperIndicatorProps>()
-const rootContext = injectStepperRootContext()
 const itemContext = injectStepperItemContext()
 useForwardExpose()
 </script>
@@ -19,9 +17,6 @@ useForwardExpose()
 <template>
   <Primitive
     v-bind="props"
-    :data-orientation="rootContext.orientation.value"
-    :data-state="itemContext.state.value"
-    :data-disabled="itemContext.disabled.value"
   >
     <slot>
       Step {{ itemContext.step.value }}
--- packages/radix-vue/src/Stepper/StepperSeparator.vue
+++ packages/radix-vue/src/Stepper/StepperSeparator.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
 import { injectStepperRootContext } from './StepperRoot.vue'
-import { injectStepperItemContext } from './StepperItem.vue'
 import { Separator, type SeparatorProps } from '@/Separator'
 import { useForwardExpose } from '@/shared'
 </script>
@@ -11,7 +10,6 @@ export interface StepperSeparatorProps extends SeparatorProps { }
 const props = withDefaults(defineProps<StepperSeparatorProps>(), {})
 
 const rootContext = injectStepperRootContext()
-const itemContext = injectStepperItemContext()
 
 useForwardExpose()
 </script>
@@ -21,8 +19,6 @@ useForwardExpose()
     v-bind="props"
     decorative
     :orientation="rootContext.orientation.value"
-    :data-linear="rootContext.linear.value"
-    :data-state="itemContext.state.value"
   >
     <slot />
   </Separator>

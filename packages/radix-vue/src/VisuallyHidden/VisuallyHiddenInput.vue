0be1ad09bc145a0e501d41f21f133867a94c22a5
tandrej <tandrej768@gmail.com>
2024-06-20 14:24:27 +0700

    Fixed issue when null value passed

--- packages/radix-vue/src/VisuallyHidden/VisuallyHiddenInput.vue
+++ packages/radix-vue/src/VisuallyHidden/VisuallyHiddenInput.vue
@@ -28,7 +28,7 @@ const parsedValue = computed(() => {
   }
 
   // if object value
-  else if (typeof props.value === 'object' && !Array.isArray(props.value)) {
+  else if (typeof props.value === 'object' && props.value !== null) {
     return Object.entries(props.value as object).map(([key, value]) => ({ name: `[${props.name}][${key}]`, value }))
   }
 

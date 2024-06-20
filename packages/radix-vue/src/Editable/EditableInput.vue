7b7da029ad429f9d83d2cc05557a6592b51c6168
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-20 09:43:30 +0300

    refactor(Editable): remove space from allowed keys and ignore meta key

--- packages/radix-vue/src/Editable/EditableInput.vue
+++ packages/radix-vue/src/Editable/EditableInput.vue
@@ -44,7 +44,7 @@ watch(context.isEditing, (value) => {
 })
 
 function handleSubmitKeyDown(event: KeyboardEvent) {
-  if ((context.submitMode.value === 'enter' && event.key === kbd.ENTER) || (context.submitMode.value === 'both' && (event.key === kbd.SPACE || event.key === kbd.ENTER) && !event.shiftKey))
+  if ((context.submitMode.value === 'enter' || context.submitMode.value === 'both') && event.key === kbd.ENTER && !event.shiftKey && !event.metaKey)
     context.submit()
 }
 </script>

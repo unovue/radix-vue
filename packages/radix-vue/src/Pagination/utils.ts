0da2fb79974b6978c0e95e775718a105a866730f
Eduard Predescu <eduard.florin.predescu@gmail.com>
2024-06-22 18:18:23 +0300

    fix(Pagination): fix right ellipsis condition and add Math.abs

--- packages/radix-vue/src/Pagination/utils.ts
+++ packages/radix-vue/src/Pagination/utils.ts
@@ -42,17 +42,17 @@ export function getRange(currentPage: number, pageCount: number, siblingCount: n
       // default condition
       = leftSiblingIndex > firstPageIndex + 2
       // if the current page is towards the end of the list
-      && lastPageIndex - itemCount - firstPageIndex + 1 > 2
+      && Math.abs(lastPageIndex - itemCount - firstPageIndex + 1) > 2
       // if the current page is towards the middle of the list
-      && leftSiblingIndex - firstPageIndex > 2
+      && Math.abs(leftSiblingIndex - firstPageIndex) > 2
 
     const showRightEllipsis
       // default condition
       = rightSiblingIndex < lastPageIndex - 2
       // if the current page is towards the start of the list
-      && lastPageIndex - itemCount > 2
+      && Math.abs(lastPageIndex - itemCount) > 2
       // if the current page is towards the middle of the list
-      && rightSiblingIndex - firstPageIndex > 2
+      && Math.abs(lastPageIndex - rightSiblingIndex) > 2
 
     if (!showLeftEllipsis && showRightEllipsis) {
       const leftRange = range(1, itemCount)

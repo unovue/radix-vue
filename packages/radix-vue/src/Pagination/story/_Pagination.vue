d69b9b4fd59c6ceab6b09b8c03369ef0c4c63db2
Andrew Kazakov <tosk-nn@yandex.ru>
2024-11-05 16:21:42 +0300

    fix(Pagination): ignore clicks if pagination button is disabled

--- packages/radix-vue/src/Pagination/story/_Pagination.vue
+++ packages/radix-vue/src/Pagination/story/_Pagination.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
-import { PaginationEllipsis, PaginationFirst, PaginationLast, PaginationList, PaginationListItem, PaginationNext, PaginationPrev, PaginationRoot, type PaginationRootProps } from '..'
+import { computed } from 'vue'
+import { PaginationEllipsis, type PaginationEllipsisProps, PaginationFirst, type PaginationFirstProps, PaginationLast, type PaginationLastProps, PaginationList, PaginationListItem, type PaginationListItemProps, type PaginationListProps, PaginationNext, type PaginationNextProps, PaginationPrev, type PaginationPrevProps, PaginationRoot, type PaginationRootProps } from '..'
 
-const props = withDefaults(defineProps<{
-  total?: PaginationRootProps['total']
-  showEdges?: PaginationRootProps['showEdges']
-}>(), {
-  total: 100,
-})
+const props = defineProps<{
+  root?: PaginationRootProps
+  list?: PaginationListProps
+
+  first?: PaginationFirstProps
+  prev?: PaginationPrevProps
+
+  listItem?: Partial<PaginationListItemProps>
+  ellipsis?: PaginationEllipsisProps
+
+  next?: PaginationNextProps
+  last?: PaginationLastProps
+}>()
+
+const rootProps = computed(() => ({ total: 100, ...props.root }))
 </script>
 
 <template>
-  <PaginationRoot v-bind="props">
+  <PaginationRoot v-bind="rootProps">
     <PaginationList
       v-slot="{ items }"
+      v-bind="props.list"
       class="flex items-center gap-2 "
     >
-      <PaginationFirst />
-      <PaginationPrev />
+      <PaginationFirst v-bind="props.first" />
+      <PaginationPrev v-bind="props.prev" />
       <template v-for="(page, index) in items">
         <PaginationListItem
           v-if="page.type === 'page'"
           :key="index"
           class="border rounded px-4 py-2 data-[selected]:bg-grass8"
           :value="page.value"
+          v-bind="props.listItem"
         >
           {{ page.value }}
         </PaginationListItem>
@@ -31,12 +43,13 @@ const props = withDefaults(defineProps<{
           :key="page.type"
           :index="index"
           class="border rounded px-4 py-2 "
+          v-bind="props.ellipsis"
         >
           &#8230;
         </PaginationEllipsis>
       </template>
-      <PaginationNext />
-      <PaginationLast />
+      <PaginationNext v-bind="props.next" />
+      <PaginationLast v-bind="props.last" />
     </PaginationList>
   </PaginationRoot>
 </template>

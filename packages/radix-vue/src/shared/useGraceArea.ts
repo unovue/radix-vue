ff134d0d70b294bd300fb976b6173e284a9e51bc
zernonia <zernonia@gmail.com>
2025-01-27 18:02:48 +0800

    fix(Tooltip): multiple tooltip display at once

--- packages/radix-vue/src/shared/useGraceArea.ts
+++ packages/radix-vue/src/shared/useGraceArea.ts
@@ -49,7 +49,7 @@ export function useGraceArea(triggerElement: Ref<HTMLElement | undefined>, conta
         const pointerPosition = { x: event.clientX, y: event.clientY }
         const hasEnteredTarget = triggerElement.value?.contains(target) || containerElement.value?.contains(target)
         const isPointerOutsideGraceArea = !isPointInPolygon(pointerPosition, pointerGraceArea.value)
-        const isAnotherGraceAreaTrigger = target.hasAttribute('data-grace-area-trigger')
+        const isAnotherGraceAreaTrigger = !!target.closest('[data-grace-area-trigger]')
 
         if (hasEnteredTarget) {
           handleRemoveGraceArea()
